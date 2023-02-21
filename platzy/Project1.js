8e231c10ced3fc89fe77eef0609e4e5d39a126c5
Yefire JS <Yefire.js@gmail.com>
2023-02-21 11:50:47 -0500

    Learning about CLOSURE

--- platzy/Project1.js
+++ platzy/Project1.js
@@ -1,1 +1,23 @@
-console.log('I can I Will I Must')
+const Papeto = 48
+console.log(`${Papeto} y`)
+
+function Hijo (){
+    const yeffer = 30
+    console.log(`${Papeto} y ${yeffer} `)
+    
+    
+    function nieto(){
+        const Christopher = 9
+        console.log(`${Papeto} y ${yeffer} y ${Christopher}`)
+        
+        function bisnieto(){
+            const Damianl = 1
+            console.log(`${Papeto} y ${yeffer} y ${Christopher} y ${Damianl}`)
+        }
+        return bisnieto()
+    }
+
+    return nieto()
+}
+
+Hijo() 
---
+++ platzy/closures.js
@@ -0,0 +1,5 @@
+function moneyBox(coins){
+    let = saveConis = 0;
+    saveConis += coins
+    console.log(`MoneyBox = ${saveConis}`)
+}
