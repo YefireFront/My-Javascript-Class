05b6a601ba53fa348e447078c4a418e97d42a145
Yefire JS <Yefire.js@gmail.com>
2023-02-22 18:59:43 -0500

    add condtional 1.2

--- 2-Control Structures/Conditionals.js
+++ 2-Control Structures/Conditionals.js
@@ -2,10 +2,22 @@
 @author MIRCHA <CONDITIONALS>
 */
 
- let a = 'Boca'
+//*CONDICIONAL BASICO
 
- if (a == 'Boca') {
-   console.log('Sos el mas grande'); 
- }else {
-    console.log('Sos un club  cualquiera');
- }
+//  let a = 'Boca'
+
+//  if (a == 'Boca') {
+//    console.log('Sos el mas grande'); 
+//  }else {
+//     console.log('Sos un club  cualquiera');
+//  }
+
+
+//*OPERADOR TERNARIO
+const team = 'boca'
+
+let soy = (team == 'boca') 
+?'Eres bostero'
+:'Eres Chico';
+
+console.log(soy);
