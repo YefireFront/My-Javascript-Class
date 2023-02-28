5a697b71b500ec8c1376a333ecd9ab34844c6ca5
Yefire JS <Yefire.js@gmail.com>
2023-02-28 10:38:26 -0500

    Clouser and acope

---
+++ 1-DataType/datatype.js
@@ -0,0 +1,6 @@
+                //! SCOPE
+//El alcance que determina la accesibilidad de las variables 
+
+//! Tipos de Scope 
+//-Global Scope -Function scope -Block scope
+//? En javaScript, los objetos y las funciones tambine son variables 
--- 2-Control Structures/Conditionals.js
+++ 2-Control Structures/Conditionals.js
@@ -1,5 +1,5 @@
 /** 
-@author MIRCHA <CONDITIONALS>
+@author JHONATAN MIRCHA <CONDITIONALS>
 */
 
 //*CONDICIONAL BASICO
@@ -14,10 +14,30 @@
 
 
 //*OPERADOR TERNARIO
-const team = 'boca'
+// const team = 'boca'
 
-let soy = (team == 'boca') 
-?'Eres bostero'
-:'Eres Chico';
+// let soy = (team == 'boca') 
+// ?'Eres bostero'
+// :'Eres Chico';
 
-console.log(soy);
+// console.log(soy);
+
+
+// if (deporte == 1){
+//     let jugador = prompt("Jugador\n -Messi \n -CR7 \n -Kilyam")
+//     if (jugador == 1){
+//         let goles =  prompt("Goles con \n 1-Derecha \n 2-Izquierda ")
+//         if (goles == 1){
+//             let RespuestaGoles = prompt('Cuantos goles crees que marco con izquierda')
+//             let finalGoles= RespuestaGoles - 1100
+//             if (finalGoles != 0){
+//                 console.log(`fallaste por ${finalGoles}`)
+//             } 
+//         }else if(goles == 2){
+//             console.log(`Los goles con izquierda son ${goles}`)
+//         }else{
+//             console.log('Opcion invalida')
+//         }
+//     }
+// }
+
