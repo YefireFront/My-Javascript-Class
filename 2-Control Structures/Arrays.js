02d38e9640b8df9f1c0b7ae36ef260f842877cc1
Yefire JS <Yefire.js@gmail.com>
2023-03-12 18:23:12 -0500

    taller

--- 2-Control Structures/Arrays.js
+++ 2-Control Structures/Arrays.js
@@ -107,10 +107,9 @@
 
 
 
-// const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,0);
+// const cum = array1.reduce( (accumulator, currentValue) => accumulator + currentValue,0);
 
-// console.log(sumWithInitial);
+// console.log(cum);
 
 
 //! REDUCE()
@@ -149,16 +148,16 @@
 
 //! SLICE()
 
-const months = ['Jan', 'March', 'April', 'June'];
-months.splice(1, 0, 'Feb');
-// Inserts at index 1
-console.log(months);
-// Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0, 'Feb');
+// // Inserts at index 1
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
 
-months.splice(4, 1, 'May');
-// Replaces 1 element at index 4
-console.log(months);
-// Expected output: Array ["Jan", "Feb", "March", "April", "May"]
+// months.splice(4, 0, 'May','June','July','August','September','October','November','December');
+// // Replaces 1 element at index 4
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "May"]
 
 
 
---
+++ Ejercices/Arrays.js
@@ -0,0 +1,186 @@
+
+// const teams =['Boca','Real Madrid','Santos','Barcelona','Racing','Boca','Santos','Barcelona',]
+const num = ['1','2','3','4','5','5',[23,32,35],'9']
+const greatsTeams =[
+    {
+        team: 'Boca',
+        titulos: 6,
+        country :'Argentina',
+        stadium : 'Bombonera',
+        motto : 'EL jugador numero 12'
+        
+    },
+    {
+        team: 'Real Madrid',
+        titulos: 14,
+        country:'España',
+        stadium : 'Bernabeu',
+        
+        
+    },
+    {
+        team: 'Santos',
+        titulos: 3,
+        country:'Brasil',
+        stadium : 'Maracana',
+        motto : 'La casa del el rey pele'
+    },
+    {
+        team: 'Barcelona',
+        titulos: 5,
+        country:'España',
+        stadium : 'Cap noun',
+        motto : 'Siempre cule'
+    },
+    {
+        team: 'Racing',
+        titulos: 1,
+        country:'Argentina',
+        stadium : 'Amalfinati'
+    },
+    {
+      team: 'Manchester United',
+      titulos: 4,
+      country:'Inglaterra',
+      stadium : 'Stanford breach'
+    },
+    {
+        team: 'Milan',
+        titulos: 6,
+        country :'Italia',
+        stadium : 'Jusepa Meatzaa'
+    },
+    {
+        team: 'LA Galaxy',
+        titulos: 1,
+        country:'US',
+        stadium : 'Dignity Health Sports Park'
+    }
+]
+
+
+//* NUMERO DE VECES QUE SE REPITE UN DATO EN UN OBJ  (Map y Reduce)
+/*
+const mapeado_Country = greatsTeams.map(k =>  k.country)
+const reducido = mapeado_Country.reduce((obj , value)=>{
+    if (obj[value]){
+        obj[value] = obj[value] + 1;
+    }else{
+        obj[value] = 1;
+    }
+    return obj;
+    
+},{})
+
+
+console.log(mapeado_Country);
+console.log(reducido);
+*/
+
+//* VALIDAR SI ALGUN DATO CUMPLE CON LA CONDICION ( MAP and SOME )
+
+/*
+const mapeado_Title = greatsTeams.map(k =>  k.titulos)
+const someado = mapeado_Title.some((k,i) => k>10  )
+console.log(mapeado_Title);
+console.log(`es ${someado} que algun elemento coincide con la condicion`);
+
+*/
+//* VALIDAR SI TODOS LOS DATOS CUMPLE CON LA CONDICION (MAP and EVERY)
+/*
+const mapeado_Name = greatsTeams.map(k =>  k.team)
+const everyado = mapeado_Name.every((k) => k.length > 3 )
+console.log(mapeado_Name);
+console.log(`es ${everyado} que todos los  elementos coinciden con la condicion`);
+*/
+
+//*  ENCONTRAR EL PRIMER ELEMENTO QUE CUMPLE CON UNA CONDICION ( find )
+
+/*
+const findneado = greatsTeams.find(country => country.country === 'US')
+console.log(`The team that macth is`);
+console.log( findneado);
+*/
+
+//*  CREATE AN ARRAY THAT IS SEPARATED BY PACE (SPLIT and JOIN)
+/*
+const bocaFrase = 'El jugador numero 12'
+let bocaFraseSplit = bocaFrase.split(' ')
+console.log(bocaFrase);
+console.log(bocaFraseSplit);
+
+let bocaFraseJoin = bocaFraseSplit.join('/')
+console.log(bocaFraseJoin);
+*/
+//*   CONVERTY 2 ARRAYS INTO ONE  (CONCAT)
+/*
+const numandstribg = num.concat(teams)
+console.log(numandstribg);
+
+*/
+//* CREATE AN ARRAY WITH THE START DATE OF THIS OBJECT
+
+const calendars = {
+    primaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita 1",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 17),
+        endDate: new Date(2021, 1, 1, 18),
+        title: "Cita 2",
+      },
+    ],
+    secondaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 12),
+        endDate: new Date(2021, 1, 1, 12, 30),
+        title: "Cita 2",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 9),
+        endDate: new Date(2021, 1, 1, 10),
+        title: "Cita 4",
+      },
+    ],
+    };
+ /*   
+    
+    TODO My solution :)
+    const pc = calendars.primaryCalendar.map(keys=>keys.startDate)
+    console.log(pc);
+    
+    const sc = calendars.secondaryCalendar.map(keys=>keys.startDate)
+    console.log(sc);
+    
+    
+    
+    TODO The Solutio :)
+    
+    const ObjToArray = Object.values(calendars)  //? convertir objeto a array 
+    console.log(ObjToArray);
+    
+    const Flatarray = ObjToArray.flat() //? Flatten the array one lvl 
+    console.log(Flatarray);
+    
+    const MapArray = Flatarray.map(keys=>keys.startDate) //? map de array and return startDate property of the objets 
+    console.log(MapArray);
+    
+
+*/
+    
+//* CAUNT THE NUMBERS OF WORD THAT REAPET IN THIS ARRAY
+
+/*
+const a = ["Beautiful is better than ugly","Explicit is better than implicit","Simple is better than complex","Complex is better than complicated",]
+
+console.log(a);
+const text = a.join(' ')
+console.log(text);
+const word = text.split(' ');
+console.log(word);
+console.log(word.length);
+  
+*/
