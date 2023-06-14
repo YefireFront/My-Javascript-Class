66ae994a85969a1a31cda3ebd9abe6269b4d599b
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-06-14 14:36:13 -0500

    Items added

---
+++ 0-ES6/error.js
@@ -0,0 +1,13 @@
+
+try {
+    console.log('Codigo a evaluar');
+    eviste
+    console.log('Codigo a evaluar segundo');
+} catch (error) {
+    console.log(`Miremo el error mejor ${error}`);
+    
+}finally{
+    console.log('el Bloque fainali se ejcutara siempre a el final de un bloque trycatch');
+
+}
+
--- 3-POO/objetos.js
+++ 3-POO/objetos.js
@@ -1,42 +1,21 @@
 const team = {
-    name : 'Boca',
-    city : 'Buenos aires',
+    name    : 'Boca',
+    city    : 'Buenos aires',
     country : 'Argentina',
     championship : [5,12,4,2],
-    sponser : {
-        2015 : 'Adidad',
-        2018 : 'Puma',
-        2021 : 'Nikwe'
-    },
-    Sing (){
+    sponser : {2015:'Adidad', 2018:'Puma', 2021:'Nikwe'},
+    Sing(){
         console.log('Que paso con el fantama del Dsenso, que paso con la gallina en san lorenzo');
+    },
+    Info(){
+        console.log(`EL equipo ${this.name}`);
     }
 }
 
 
 
 console.log(team.name);
-console.log(team.sponser[2015]);
 console.log(team.championship);
-
+console.log(team.sponser[2015]);
 team.Sing()
-
-// let p = 0
-// while (p < 100) {
-//     console.log(p);
-//     p++
-// }
-
-
-function queso (argu){
-
-    console.log(argu*2);
-}
-
-
-for (let index = 0; index < 10; index++) {
-    console.log(index);
-
-    queso(index)
-    
-}
+team.Info()
