79f14e31bf03e9c98fbbe452ae22b9546078fc4b
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-07-12 21:19:16 -0500

    adding

---
+++ m.js
@@ -0,0 +1,15 @@
+let nombre_Variable = 99
+const Variable = "Boca jrs de Argentina"
+
+
+console.log("Mensaje que aparezco en consola");
+console.info('Mensaje con console tipo error');
+console.warn('Mensaje de consola con variable',Variable);
+console.error(`Mensaje con otra forma de uniir variabel ${Variable}`);
+
+if (Variable == Yeffer) {
+    console.log("Amamos el futbol");
+}else{
+    console.info('Amamos el basquet');
+}
+
