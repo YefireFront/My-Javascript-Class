ecb87e1ea26ceb5253b58e8ead2d894ae2fed7f5
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-07-31 11:08:57 -0500

    update 13/07

---
+++ 3-POO/Fazt.js
@@ -0,0 +1,28 @@
+console.log(typeof true);
+console.log("Yefire".toUpperCase());
+console.log(typeof 9);
+
+console.log(typeof []);
+console.log(typeof {});
+
+const team = {
+    name    : 'Boca',
+    city    : 'Buenos aires',
+    country : 'Argentina',
+    championship : [5,12,4,2],
+    sponser : {2015:'Adidad', 2018:'Puma', 2021:'Nikwe'},
+    Sing(){
+        console.log('Que paso con el fantama del Dsenso, que paso con la gallina en san lorenzo');
+    },
+    Info(){
+        console.log(`EL equipo ${this.name}`);
+    }
+}
+
+
+
+console.log(team.name);
+console.log(team.championship);
+console.log(team.sponser[2015]);
+team.Sing()
+team.Info()
