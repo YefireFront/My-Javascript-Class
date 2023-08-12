2fb894af532631f142d8921dd6de1a7bfa337f48
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-08-12 16:02:16 -0500

    Update Fer.js

--- 3-POO/Fer.js
+++ 3-POO/Fer.js
@@ -12,4 +12,18 @@ let nba ={
 
 }
 
+let nba2 ={
+    name : 'Jordan',
+    team : 'Bulss',
+    city : 'Chocago',
+    championship : [1992, 1994, 1995, 1996],
+    directors : {
+        president : 'russel wats',
+        manage : 'Crabs Bilss',
+        sponsore: 'Nike'
+    },
+    dinasty : true
+
+}
+
 console.log(`los campeonatos de ${nba.name} fueron en ${nba.championship}`);
