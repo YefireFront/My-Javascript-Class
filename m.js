378e7a270782ec4410c0e487d0e920953675467e
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-08-15 21:02:50 -0500

    Update m.js

--- m.js
+++ m.js
@@ -1,7 +1,20 @@
-let persona = {
-    nombre : 'Yeffer',
-    apellido : 'Anilio',
-    imprimi : function (params) {
-        
+function esPrimo(numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let contadorPrimos = 0;
+
+for (let numero = 2; contadorPrimos < 20; numero++) {
+    if (esPrimo(numero)) {
+        console.log(numero);
+        contadorPrimos++;
     }
 }
