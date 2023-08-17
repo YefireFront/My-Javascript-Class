6b0e8dcf1c2f4529dfebdd4729913c553d9c1051
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-08-17 14:25:33 -0500

    update Meth

--- m.js
+++ m.js
@@ -1,11 +1,30 @@
-let persona = {
-    nombre : 'Yeffer',
-    apellido : 'Anilio',
-    imprimi : function (params) {
-        
-    }
+function formatoFecha(fecha) {
+    const partes = fecha.split("-");
+    const fechaFormateada = partes[2] + "/" + partes[1] + "/" + partes[0];
+    // const fechaFormateada = [partes[2], partes[1], partes[0]].join("/");
+    return fechaFormateada;
+}
+
+const fechaOriginal = "2023-08-15";
+const fechaFormateada = formatoFecha(fechaOriginal);
+
+// console.log("Fecha Original:", fechaOriginal);
+// console.log("Fecha Formateada:", fechaFormateada);
+
+
+let AP = [];
+let acum = "";
+for (let i = 0; i < fechaOriginal.length; i++) {
     acum += fechaOriginal[i]
+    if (fechaOriginal[i] == '-') {
+        AP[AP.length] = acum
+        acum = ""
+    }
 }
-if (acum != "") {
-    arraynuevofecha[arraynuevofecha.length]=acum
+if (acum !== "") {
+    AP[AP.length] = acum;
 }
+console.log(AP);
+
+let fin = AP[2] + AP[1] + AP[0]
+console.log(fin);
