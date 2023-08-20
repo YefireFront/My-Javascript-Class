0297a48c35ffa1aedd2b81255c5c5c9fdb322170
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-08-19 21:40:24 -0500

    Update Fer.js

--- 3-POO/Fer.js
+++ 3-POO/Fer.js
@@ -26,4 +26,31 @@ let nba2 ={
 
 }
 
+const team = {
+    name    : 'Boca',
+    city    : 'Buenos aires',
+    country : 'Argentina',
+    championship : [5,12,4,2],
+    sponser : {2015:'Adidad', 2018:'Puma', 2021:'Nikwe'},
+    Sing(){
+        console.log('Que paso con el fantama del Dsenso, que paso con la gallina en san lorenzo');
+    },
+    Info(){
+        console.log(`EL equipo ${this.name}`);
+    },
+    champ(){
+        
+        this.championship.forEach(element => {
+            console.log(element);
+        });
+    }
+}
+
+
+
+console.log(team.name);
+console.log(team.championship);
+console.log(team.sponser[2015]);
+team.champ()
+
 console.log(`los campeonatos de ${nba.name} fueron en ${nba.championship}`);
