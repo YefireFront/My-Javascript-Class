3e339c195d1db95e6db7014e139ca31ef5590712
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-08-20 12:50:39 -0500

    Update objetos.js

--- 3-POO/objetos.js
+++ 3-POO/objetos.js
@@ -1,26 +1,43 @@
-const team = {
-    name    : 'Boca',
-    city    : 'Buenos aires',
-    country : 'Argentina',
-    championship : [5,12,4,2],
-    sponser : {2015:'Adidad', 2018:'Puma', 2021:'Nikwe'},
-    Sing(){
-        console.log('Que paso con el fantama del Dsenso, que paso con la gallina en san lorenzo');
-    },
-    Info(){
-        console.log(`EL equipo ${this.name}`);
-    },
-    champ(){
-        
-        this.championship.forEach(element => {
-            console.log(element);
-        });
+class Jugador {
+    constructor(nombre, posicion) {
+      this.nombre = nombre;
+      this.posicion = posicion;
     }
-}
-
-
-
-console.log(team.name);
-console.log(team.championship);
-console.log(team.sponser[2015]);
-team.champ()
+  
+    presentarse() {
+      console.log(`Hola, soy ${this.nombre} y juego como ${this.posicion}.`);
+    }
+  }
+  
+  class EquipoFutbol {
+    constructor(nombre) {
+      this.nombre = nombre;
+      this.jugadores = [];
+    }
+  
+    agregarJugador(jugador) {
+      this.jugadores.push(jugador);
+      console.log(`${jugador.nombre} ha sido añadido al equipo ${this.nombre}.`);
+    }
+  
+    presentarEquipo() {
+      console.log(`Equipo: ${this.nombre}`);
+      console.log("Jugadores:");
+      this.jugadores.forEach(jugador => {
+        console.log(`- ${jugador.nombre} (${jugador.posicion})`);
+      });
+    }
+  }
+  
+  // Crear jugadores
+  const jugador1 = new Jugador('Lionel Messi', 'Delantero');
+  const jugador2 = new Jugador('Sergio Ramos', 'Defensor');
+  
+  // Crear equipo de fútbol
+  const equipoBarcelona = new EquipoFutbol('FC Barcelona');
+  equipoBarcelona.agregarJugador(jugador1);
+  equipoBarcelona.agregarJugador(jugador2);
+  
+  // Presentar equipo y jugadores
+  equipoBarcelona.presentarEquipo();
+  
