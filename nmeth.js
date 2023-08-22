2d3b2343a32ccd928819fbc30c94535dfcf563bb
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-08-22 16:59:16 -0500

    update

---
+++ nmeth.js
@@ -0,0 +1,35 @@
+// const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+// numbers.sort();
+// console.log(numbers);
+
+// const Boca = ['Gati', 'Navarro', 'Serna', 'Riquelme', 'aaaa']
+// Boca.reverse()
+// console.log(Boca);
+// // Boca.sort()
+// // console.log(Boca);
+
+// let song = "Que paso con el fantasma del descenso"
+// let yef = song.replace('fantasma', 'ghost')
+// console.log(song);
+// console.log(yef);
+
+
+// const arr = [9, 52, 61, 8, 1, 9, 2, 9]
+// let ant = 0
+// let pos = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > arr[i + 1]) {
+//         ant = arr[i]
+//         pos = arr[i + 1]
+//         // chgange
+//         arr[i] = pos
+//         arr[i + 1] = ant
+//         i = 0
+
+//     }
+// }
+// console.log(arr);
+
+console.log(typeof(67));
+console.log(typeof('Yeffre'));
+console.log(typeof(true));
