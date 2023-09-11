4c6954e3e8837d5d03c3c147524e14cb9ec57415
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-09-10 19:37:10 -0500

    Update Sync.js

--- 4-Synchrony/Sync.js
+++ 4-Synchrony/Sync.js
@@ -1,14 +1,4 @@
-async function myAsyncFunction() {
-    // Asynchronous code here
-  }
-  
 
-  async function fetchData() {
-    const response = await fetch('https://example.com/api/data');
-    const data = await response.json();
-    return data;
-  }
-  
 
   // Definimos una función asíncrona que realiza una solicitud HTTP
 async function obtenerDatos() {
@@ -40,5 +30,42 @@ async function obtenerDatos() {
     })
     .catch(error => {
       console.error('Error al obtener datos:', error);
+    });
+  
+
+    // Definimos una función asíncrona para obtener información de un Pokémon
+async function obtenerInfoPokemon(pokemonName) {
+    try {
+      // Construimos la URL de la API con el nombre del Pokémon
+      const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`;
+      
+      // Hacemos una solicitud GET a la API de Pokémon
+      const respuesta = await fetch(apiUrl);
+      
+      // Verificamos si la solicitud fue exitosa
+      if (!respuesta.ok) {
+        throw new Error('No se pudo obtener la información del Pokémon');
+      }
+      
+      // Parseamos la respuesta a JSON
+      const datosPokemon = await respuesta.json();
+      
+      // Devolvemos los datos del Pokémon
+      return datosPokemon;
+    } catch (error) {
+      // Manejamos los errores
+      console.error('Se produjo un error:', error);
+      throw error; // Lanzamos el error nuevamente para que pueda ser manejado externamente si es necesario
+    }
+  }
+  
+  // Llamamos a la función asíncrona para obtener información sobre un Pokémon
+  const nombrePokemon = 'pikachu'; // Cambia el nombre del Pokémon según tu elección
+  obtenerInfoPokemon(nombrePokemon)
+    .then(data => {
+      console.log(`Información de ${nombrePokemon.toUpperCase()}:`, data);
+    })
+    .catch(error => {
+      console.error(`Error al obtener información de ${nombrePokemon.toUpperCase()}:`, error);
     });
   
