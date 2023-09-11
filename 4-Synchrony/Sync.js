3c9c1728b8c372260f126f971dad55a25fdfbd08
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-09-10 19:38:30 -0500

    Update Sync.js

--- 4-Synchrony/Sync.js
+++ 4-Synchrony/Sync.js
@@ -1,38 +1,5 @@
 
 
-  // Definimos una función asíncrona que realiza una solicitud HTTP
-async function obtenerDatos() {
-    try {
-      // Hacemos una solicitud GET a una API
-      const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-      
-      // Verificamos si la solicitud fue exitosa
-      if (!respuesta.ok) {
-        throw new Error('No se pudo obtener la información');
-      }
-      
-      // Parseamos la respuesta a JSON
-      const datos = await respuesta.json();
-      
-      // Devolvemos los datos obtenidos
-      return datos;
-    } catch (error) {
-      // Manejamos los errores
-      console.error('Se produjo un error:', error);
-      throw error; // Lanzamos el error nuevamente para que pueda ser manejado externamente si es necesario
-    }
-  }
-  
-  // Llamamos a la función asíncrona y manejamos los datos
-  obtenerDatos()
-    .then(data => {
-      console.log('Datos obtenidos:', data);
-    })
-    .catch(error => {
-      console.error('Error al obtener datos:', error);
-    });
-  
-
     // Definimos una función asíncrona para obtener información de un Pokémon
 async function obtenerInfoPokemon(pokemonName) {
     try {
@@ -60,7 +27,7 @@ async function obtenerInfoPokemon(pokemonName) {
   }
   
   // Llamamos a la función asíncrona para obtener información sobre un Pokémon
-  const nombrePokemon = 'pikachu'; // Cambia el nombre del Pokémon según tu elección
+  const nombrePokemon = 'pichu'; // Cambia el nombre del Pokémon según tu elección
   obtenerInfoPokemon(nombrePokemon)
     .then(data => {
       console.log(`Información de ${nombrePokemon.toUpperCase()}:`, data);
