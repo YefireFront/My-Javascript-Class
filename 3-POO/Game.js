26c257cba91d7de4de378ebdd670a99cc7244a5c
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-09-12 20:32:12 -0500

    Create Game.js

---
+++ 3-POO/Game.js
@@ -0,0 +1,13 @@
+
+
+function Personaje(name , type , ) {
+    this.live = 100,
+    this.magic = 100
+    this.name = name,
+    this.type = type
+}
+
+
+let yeffer = new Personaje ('Yeffer', 'Cabalero')
+
+
