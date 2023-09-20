b55c9ada8ace25fbf978040f4819603edd94f9d0
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-09-20 08:38:56 -0500

    Update Arrays.js

--- Ejercices/Arrays.js
+++ Ejercices/Arrays.js
@@ -184,3 +184,68 @@ console.log(word);
 console.log(word.length);
   
 */
+
+
+const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+numbers.sort();
+console.log(numbers);
+
+const Boca = ['Gati', 'Navarro', 'Serna', 'Riquelme', 'aaaa']
+Boca.reverse()
+
+let song = "Que paso con el fantasma del descenso"
+let yef  = song.replace('fantasma', 'ghost')
+
+
+const arr = [9, 52, 61, 8, 1, 9, 2, 9]
+let ant = 0
+let pos = 0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+        ant = arr[i]
+        pos = arr[i + 1]
+        // chgange
+        arr[i] = pos
+        arr[i + 1] = ant
+        i = 0
+
+    }
+}
+console.log(arr);
+
+console.log(typeof(67));
+console.log(typeof('Yeffre'));
+console.log(typeof(true));
+
+function formatoFecha(fecha) {
+    const partes = fecha.split("-");
+    const fechaFormateada  = partes[2] + "/" + partes[1] + "/" + partes[0];
+    // const fechaFormateada = [partes[2], partes[1], partes[0]].join("/");
+    return fechaFormateada;
+}
+
+const fechaOriginal = "2023-08-15";
+const fechaFormateada = formatoFecha(fechaOriginal);
+
+// console.log("Fecha Original:", fechaOriginal);
+// console.log("Fecha Formateada:", fechaFormateada);
+
+
+let AP = [];
+let acum = "";
+for (let i = 0; i < fechaOriginal.length; i++) {
+    if (fechaOriginal[i] == '-') {
+        AP[AP.length] = acum + ''
+        acum = ""
+        continue
+        
+    }
+    acum += fechaOriginal[i]
+}
+if (acum !== "") {
+    AP[AP.length] = acum;
+}
+console.log(AP);
+
+let fin = AP[2] + '/'+ AP[1] + '/'+ AP[0]
+console.log(fin);
