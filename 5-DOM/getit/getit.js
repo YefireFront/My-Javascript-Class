dd11a30a9bc529aa60d23af83d89c504d6561080
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-09-21 13:32:43 -0500

    dom2.0

---
+++ 5-DOM/getit/getit.js
@@ -0,0 +1,34 @@
+// const n = document.getElementById('casa')
+// console.log(n);
+
+// const nn = document.querySelector('#casa');
+// console.log(nn);
+
+// const e = document.querySelectorAll('li')
+// console.log(e);
+
+// e.forEach(e => {
+
+//     console.log(e);
+    
+// });
+
+
+// let NodeaArray = Array.from(e)
+//  console.log(NodeaArray);
+ 
+//  NodeaArray.pop()
+//  NodeaArray.shift()
+//  console.log(NodeaArray);
+
+
+
+ const $header = document.querySelector('.headre')
+ console.log($header);
+
+
+ const $hijos1 = document.querySelector('.header__1')
+ console.log($hijos1);
+
+
+ $hijos1.onclick = () => {$header.classList.toggle('bord');}
