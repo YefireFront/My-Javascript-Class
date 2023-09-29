51cdf08ec33c9f0331832a39ab0e488659a33eb8
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-09-29 11:11:04 -0500

    update

--- 0-ES6/closures.js
+++ 0-ES6/closures.js
@@ -1,62 +1,93 @@
 /** 
 @author PLATZY -- OSCAR BARAJAS  <CLOSURE>
 */
-//!------------------------------------------CLOUSERE
+
 //* Permite accedre a el ambito de una funcion exterior desde una funcion interior 
-//* Se tiene una Clusure cuando una funcion accede a una vareiabnle fuera de su contexto 
-
-//?todo = No se aplica clouser
-function moneyBox(coins){
-    let saveConis = 0;
-    saveConis += coins
-    console.log(`MoneyBox = ${saveConis}`)
-}
-moneyBox(5)
-moneyBox(15)
+//* Se tiene una Clusure cuando una funcion accede a una vareiable fuera de su contexto 
 
 
-function Cajamoney(){
-    let saveConis = 0 ;
-    function countConis(coins){
-        saveConis += coins;
-        console.log(` Caja Money: $${saveConis}`)
+// function moneyBox(coins){
+//     let saveConis = 0;
+//     saveConis += coins
+//     console.log(`MoneyBox = ${saveConis}`)
+// }
+// moneyBox(5)
+// moneyBox(15)
+
+
+// function Cajamoney(){
+//     let saveConis = 0 ;
+//     function countConis(coins){
+//         saveConis += coins;
+//         console.log(` Caja Money: $${saveConis}`)
+//     }
+//     return countConis
+
+// }
+
+//  const MyCajamoney = Cajamoney();
+//  const yefferMoney = Cajamoney();
+
+ 
+//  MyCajamoney(5);
+//  yefferMoney(100)
+//  MyCajamoney(5);
+ 
+//  yefferMoney(100)
+//  MyCajamoney(5);
+
+ 
+
+// function veterinaria (){
+//     let animal =[]
+
+//    function addpet(pet){
+//     animal.push(pet)
+//     console.log(`Veterianrie List ${animal} and the last pet add is ${pet}`)
+
+//    }
+//    return addpet
+// }
+
+// const MarchPets = veterinaria()
+//  MarchPets('PERRO')
+//  MarchPets('GATOS')
+
+
+//  function exterior() {
+//     var variableExterior = 10;
+  
+//     function interior(p) {
+//       console.log(variableExterior + p);
+//     }
+  
+//     return interior;
+//   }
+  
+//   let clausura = exterior();
+//   let clausura2 = exterior();
+  
+//   clausura(0); // Esto imprimirá "10" en la consola
+//   clausura(15); // Esto imprimirá "10" en la consola
+
+  
+
+
+/** 
+@author LA COCINA DEL CODIGO  <CLOSURE>
+*/
+
+
+function countCrator() {
+    let cot = 0
+
+    setTimeout(()=>{
+        cot += 100 
+    }, 5000)
+
+    return function increase () {
+        return cot++
     }
-    return countConis
-
 }
 
- const MyCajamoney = Cajamoney();
- const yefferMoney = Cajamoney();
-
- 
- MyCajamoney(5);
- yefferMoney(100)
- MyCajamoney(5);
- 
- yefferMoney(100)
- MyCajamoney(5);
-
- 
-
-function veterinaria (){
-    let animal =[]
-
-   function addpet(pet){
-    animal.push(pet)
-    console.log(`Veterianrie List ${animal} and the last pet add is ${pet}`)
-
-   }
-   return addpet
-}
-
-const MarchPets = veterinaria()
- MarchPets('PERRO')
- MarchPets('GATOS')
-
-
-
-function Yeffer (Somos, Venimos) {
-    Somos += Venimos;
-    console.log(`Yeffer: ${Somos}`)
-}
-
+firscounter = countCrator()
