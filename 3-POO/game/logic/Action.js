ebf145940f1c77c0907ee09ffdd4cecc3c1d0880
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-10-12 15:13:20 -0500

    12S

---
+++ 3-POO/game/logic/Action.js
@@ -0,0 +1,4 @@
+const $skill1 = document.querySelector("#APP > div > div.card.Yeffer > div.card__info > div > i.fa-solid.fa-user-shield")
+console.log($skill1);
+
+$skill1.addEventListener('click',)
--- 3-POO/game/logic/Built.js
+++ 3-POO/game/logic/Built.js
@@ -1,185 +1,4 @@
 
-class Personaje {
-  constructor({ name, atk, def }) {
-    this.name = name;
-    this.life = 100;
-    this.atk = atk;
-    this.def = def;
-    this.energy = 100;
-  }
-  dead(objetivo = false) {
-    if (objetivo.life <= 0 || this.life <= 0) {
-      console.log(`Estas intentando interactuar con personajes muertos`);
-      return true;
-    }
-  }
-  exhausted() {
-    if (this.energy < 70) return true;
-  }
-  tired() {
-    if (this.energy < 30) return true;
-  }
-  Atacar(objetivo) {
-    if (this.dead(objetivo)) return false;
-
-
-    if (this.atk > objetivo.def) {
-
-      objetivo.life = objetivo.life - (this.atk - objetivo.def);
-
-      if (objetivo.life < 0) {
-        objetivo.life = 0;
-      }
-    } else {
-      console.log(
-        `Tu ataque es de ${this.atk} y la defensa del objetivo es mayor, es de ${objetivo.def} no le causas daño`
-      );
-
-    }
-    
-    if (this.constructor.name == 'Warrior') {
-        this.special++
-      }
-      Reloade(this, objetivo);
-  }
-}
-
-class Warrior extends Personaje {
-  constructor({ name, atk, def }) {
-    super({ name, atk, def });
-    this.special = 0;
-  }
-
-  blindar() {
-    if (this.dead()) return false;
-    if (this.tired()) return false;
-    this.def += 15;
-    this.energy -= 30;
-
-    console.log(`El personaje ${this.name} Activa Blindar`);
-    console.log(this);
-    Reloade(this)
-    
-
-  }
-
-  iraYopuka(objetivo) {
-    if (this.dead(objetivo)) return false;
-    if (this.exhausted()) return false;
-    let dif = this.atk - objetivo.def;
-    if (dif > 30) {
-      objetivo.life = 0;
-      this.energy -= 70;
-      Reloade(this,objetivo)
-    }
-  }
-}
-
-class Healer extends Personaje{
-  constructor({name,atk,def}){
-    super({name , atk, def});
-    this.special = 1;
-  };
-
-  curar = function (objetivo) {
-    if (this.dead())  return false;
-    if (this.tired()) return false;
-  
-    objetivo.life += 30;
-    this.energy -= 30;
-    if (objetivo.life > 100) {
-      objetivo.life = 100;
-    }
-
-    Reloade(this,objetivo)
-  };
-
-  sacrificio = function (objetivo) {
-      if (this.dead())      return false;
-      if (this.exhausted()) return false;
-
-      let cantidad = 100 - objetivo.life
-      if (cantidad < this.life){
-          objetivo.life += cantidad
-          this.life     -= cantidad;
-          this.energy   -= 70 
-      }else{
-          objetivo.life += this.life 
-          this.life = 1
-          this.energy   -= 70 
-      }
-
-      Reloade(this,objetivo)
-
-
-  };
-
-  revivir = function (objetivo){
-
-      if (this.special == 0) return false;
-      if (this.life === 0)   return false;  
-      if (objetivo.life > 0) return false;            
-      if (this.exhausted())  return false;
-
-      objetivo.life = 40
-      this.energy   -=70
-      this.special = 0
-
-      Reloade(this,objetivo)
-
-
-
-  }
-
-
-
-
-}
-
-class Witcher extends Personaje{
-  constructor({name,atk,def}){
-    super({name,atk,def})
-    this.special = 1
-  }
-
-  Mejora = function (objetivo) {
-    if (this.energy >= 30) {
-      this.energy -= 30;
-      objetivo.atk += 10;
-      objetivo.def += 10;
-      if (objetivo.def > 70) {
-        objetivo.def = 70;
-      }
-      if (objetivo.atk > 100) {
-        objetivo.atk = 100;
-      }
-    } else {
-      console.log("energy Insuficiente");
-    }
-
-    Reloade(this,objetivo)
-  };
-}
-
-
-
-
-const yeffer = new Warrior({
-  name: "Yeffer",
-  atk: 70,
-  def: 60,
-});
-const Arley = new Witcher({
-  name: "Arley",
-  atk: 50,
-  def: 20,
-});
-const Walter = new Healer({
-  name: "Walter",
-  atk: 80,
-  def: 40,
-});
-
 
 
 const PersonajeGame = [Arley,yeffer,Walter];
@@ -187,28 +6,78 @@ const allplayers = []
 
 PersonajeGame.forEach((pj) => {
 
-  //? CREANTING IMG  
+  //% CREANTING IMG  
   const div__img = document.createElement("div");
   const div_image = document.createElement("img");
-  //$ PARSEANDO
+  //% PARSEANDO
   div__img.classList.add("card__img");
   //% INSERTANDO
   div__img.append(div_image);
-  //! DATA
+  //% DATA
   div_image.src = `./img/${pj.constructor.name}.png`
 
 
 
-  //? CREANTING CARD INFO
+  //% CREANTING CARD INFO
   const div__info = document.createElement("div");
-  const info_h2 = document.createElement("h2");
-  const info_p = document.createElement("p");
-  //! DATA
+  const info_h2   = document.createElement("h2");
+  const $skills   = document.createElement("div");
+  const $skill1   = document.createElement("i");
+  const $skill2   = document.createElement("i");
+  const $skill3   = document.createElement("i");
+  const $skill4   = document.createElement("i");
+  //% INTRODUCING TEXT
   info_h2.textContent= pj.name
-  //$ PARSEANDO
+
+  //% ADDING CLASS
   div__info.classList.add("card__info");
-  //% INSERTANDO
-  div__info.append(info_h2, info_p);
+  
+  if(pj.constructor.name == 'Healer'){
+    $skills.classList.add('skills','skills-Healer')
+    
+    $skill1.classList.add('fa-sharp','fa-solid','fa-wand-sparkles')
+    $skill2.classList.add('fa-solid' ,'fa-hand-holding-medical')
+    $skill3.classList.add('fa-sharp', 'fa-solid', 'fa-notes-medical')
+    $skill4.classList.add('fa-brands', 'fa-gratipay')
+    
+  }
+  if(pj.constructor.name == 'Warrior'){
+    $skills.classList.add('skills', 'skills-Warrior')
+
+    $skill1.classList.add('fa-solid','fa-hand-fist')
+    $skill2.classList.add('fa-solid' ,'fa-user-shield')
+    $skill3.classList.add('fa-solid','fa-dragon')
+    $skill4.classList.add('fa-solid' ,'fa-people-line')
+    
+  }
+  if(pj.constructor.name == 'Witcher'){
+    $skills.classList.add('skills', 'skills-Witcher')
+
+    $skill1.classList.add('fa-sharp','fa-solid','fa-wand-sparkles')
+    $skill2.classList.add('fa-solid' ,'fa-virus')
+    $skill3.classList.add('fa-solid' ,'fa-gears')
+    $skill4.classList.add('fa-solid','fa-skull-crossbones', 'neon-icon')
+    
+  }
+
+ //% EVENTS
+  if(pj.constructor.name == 'Warrior'){
+
+
+    $skill2.addEventListener("click",()=>{
+      pj.blindar()
+    })
+
+    
+  }
+
+
+ 
+
+
+  //% INSERTIND NODES
+  $skills.append($skill1,$skill2,$skill3,$skill4)
+  div__info.append(info_h2,$skills);
   
 
 
@@ -332,11 +201,3 @@ function Reloade(me , you) {
   
 
 
-
-
-
-
-yeffer.Atacar(Walter)
-yeffer.Atacar(Arley)
-yeffer.Atacar(Arley)
-Walter.revivir(Arley)
