f8b93c2edb0fc097eda369695e84284f76822a68
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-10-19 11:16:19 -0500

    upd

--- 4-Synchrony/APIs/NBA/nba.js
+++ 4-Synchrony/APIs/NBA/nba.js
@@ -1,15 +1,17 @@
-const apiUrl = 'https://www.balldontlie.io/api/v1/players';
+const apiUrl = 'https://www.openligadb.de/api/getTeamsByLeagueName/bl1/2022';
 
-// Realiza una solicitud para obtener datos de jugadores
+// Realiza una solicitud para obtener datos de equipos de la Premier League
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    const players = data.data;
-    players.forEach(player => {
-      console.log('Nombre del jugador:', player.first_name, player.last_name);
-      console.log('Imagen del jugador:', player.team.full_name, player.position);
+    const teams = data;
+    teams.forEach(team => {
+      console.log('Nombre del equipo:', team.TeamName);
+      console.log('País:', team.ShortName);
+      console.log('Estadio:', team.StadiumName);
+      console.log('----------------------------------------');
     });
   })
   .catch(error => {
-    console.error('Hubo un error al obtener los datos de los jugadores:', error);
+    console.error('Hubo un error al obtener los datos de los equipos de la Premier League:', error);
   });
