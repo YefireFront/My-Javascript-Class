9ab94497891fb5eea643daeca0fd2a93f88febba
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-10-29 19:47:47 -0500

    Update Taller.js

--- 4-Synchrony/promise/Taller.js
+++ 4-Synchrony/promise/Taller.js
@@ -8,9 +8,6 @@ function rasenganPromise() {
   });
 }
 
-rasenganPromise().then((result) => {
-//   console.log(result);
-});
 
 //* Crea una función que tome un número como parámetro y devuelva una promesa que resuelva con ese número duplicado.
 
