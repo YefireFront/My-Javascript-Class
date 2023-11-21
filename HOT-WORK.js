88aa592ac3971135f6bc4095ff2120b2d8762213
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-11-21 16:49:58 -0500

    upd

--- HOT-WORK.js
+++ HOT-WORK.js
@@ -1,18 +1,63 @@
-
-function esPrimo(numero) {
-    for (let i = 2; i < numero; i++) {
-        if (numero % i === 0) {
-            return false;
-        }
+const dishData = [
+    {
+      name: "Italian pasta",
+      price: 9.55
+    },
+    {
+      name: "Rice with veggies",
+      price: 8.65
+    },
+    {
+      name: "Chicken with potatoes",
+      price: 15.55
+    },
+    {
+      name: "Vegetarian Pizza",
+      price: 6.45
+    },
+  ];
+  const tax = 1.20;
+  
+  // Implement getPrices()
+  function getPrices(taxBoolean) {
+    for (let i = 0; i < dishData.length; i++) {
+      let finalPrice;
+      const dish = dishData[i];
+  
+      if (taxBoolean === true) {
+        finalPrice = dish.price * tax;
+      } else if (taxBoolean === false) {
+        finalPrice = dish.price;
+      } else {
+        console.log("You need to pass a boolean to the getPrices call!");
+        return;
+      }
+  
+      console.log(`Dish: ${dish.name}, Price: $${finalPrice.toFixed(2)}`);
     }
-    return true;
-}
-
-
-let contadorPrimos = 0;
-for (let numero = 2; contadorPrimos < 20; numero++) {
-    if (esPrimo(numero)) {
-        console.log(numero);
-        contadorPrimos++;
+  
+    return "Function execution complete!";
+  }
+  
+  // Implement getDiscount()
+  function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+  
+    if (typeof guests === 'number' && guests > 0 && guests < 30) {
+      let discount = 0;
+      if (guests < 5) {
+        discount = 5;
+      } else if (guests >= 5) {
+        discount = 10;
+      }
+  
+      console.log('Discount is: $' + discount);
+    } else {
+      console.log('The second argument must be a number between 0 and 30');
     }
-}
+  }
+  
+  // Call getDiscount() with examples
+  getDiscount(true, 2);
+  getDiscount(false, 10);
+  
