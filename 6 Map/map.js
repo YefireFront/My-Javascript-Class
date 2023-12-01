c27681051a95409b788cd66c9d739ae6e00d53dd
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-11-30 23:00:38 -0500

    Update map.js

--- 6 Map/map.js
+++ 6 Map/map.js
@@ -1,7 +1,7 @@
 const miMapa = new Map();
 miMapa.set('a', 1);
 miMapa.set('b', 2);
-miMapa.set('c', 3);
+miMapa.set('c', 32);
 
 // Iterar sobre un mapa y mostrar claves y valores utilizando for...of y entries()
 for (const [clave, valor] of miMapa.entries()) {
