fba730ea8db816da3de5c1fa5620e136e29b056e
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-12-11 11:16:40 -0500

    Update Bucles.js

--- 2-Control Structures/Bucles.js
+++ 2-Control Structures/Bucles.js
@@ -3,3 +3,15 @@
 */
 
 
+array.forEach(element => {
+    
+});
+
+
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
