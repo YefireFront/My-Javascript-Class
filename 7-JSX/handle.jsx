d3b1c7daa2c91256d04646b77f4f01fba43d0c97
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2023-12-11 14:49:21 -0500

    ups App2

---
+++ 7-JSX/handle.jsx
@@ -0,0 +1,22 @@
+const { useState } = require("react");
+
+
+ function clickHandler(){
+    console.log('welcome ti the jungle');
+ }
+
+ const fnArrow = ()=>{
+    console.log(`vengo de arrow`);
+ }
+
+<button 
+className="freach"
+id="yeffer"
+onClick={()=>{
+    clickHandler()
+    fnArrow()
+}}
+>Click me </button>
+
+
+const [yeffer, setYeffer] = useState(22)
