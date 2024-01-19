941a64daac2fd40f953f4cba80a8d51278bef418
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-01-18 22:48:57 -0500

    Update HOT-WORK.js

--- HOT-WORK.js
+++ HOT-WORK.js
@@ -75,6 +75,7 @@ const dishData = [
   console.log(`Day 1. This is first day of the rest of my life, the day when everything changes` )
   console.log(`Day 2. One day at a time` )
   console.log(`Day 3. Big Decition` )
+  console.log(`Day 4. New House, New Moment` )
 
   
 
