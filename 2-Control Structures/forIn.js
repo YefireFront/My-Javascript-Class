e4c431d54120b155245a699b9aa3a91e4b481d56
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-01-25 22:02:03 -0500

    Update forIn.js

--- 2-Control Structures/forIn.js
+++ 2-Control Structures/forIn.js
@@ -17,14 +17,3 @@ for (let indice in numeros) {
 
 
 
-
-const libros = [
-    { titulo: 'El señor de los anillos', autor: 'J.R.R. Tolkien' },
-    { titulo: 'Harry Potter', autor: 'J.K. Rowling' },
-    { titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez' }
-  ];
-  
-  for (let index in libros) {
-    console.log('Libro ' + (parseInt(index) + 1) + ': ' + libros[index].titulo);
-  }
-  
