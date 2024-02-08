34f9dc8d2797e42d3098baa0adb8f2296af08f67
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-02-08 11:48:45 -0500

    upd

--- Prueba Tecnica/semiSenior.js
+++ Prueba Tecnica/semiSenior.js
@@ -124,3 +124,16 @@ function cambiarColorDeFondo() {
 }
 
 cambiarColorBtn.addEventListener('click', cambiarColorDeFondo);
+
+/*
+Ejercicio: Sistema de gestión de tareas
+Crea un sistema de gestión de tareas que permita al usuario agregar nuevas tareas, marcar tareas como completadas
+eliminar tareas y ver todas las tareas existentes. Utiliza un objeto para representar cada tarea, con propiedades como id, descripcion y completada.
+
+Requisitos:
+
+Debe haber funciones para agregar, marcar como completada, eliminar y mostrar todas las tareas.
+Las tareas deben tener un identificador único (id).
+Debe manejar el estado de completado de cada tarea.
+Debe proporcionar una interfaz de usuario simple para que el usuario interactúe con el sistema de gestión de tareas.
+*/
