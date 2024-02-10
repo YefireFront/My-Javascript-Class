e61f70b9b4023167f40f4909d28b4c845c6957f1
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-02-10 11:58:51 -0500

    new topic

---
+++ Exception Handling/try.js
@@ -0,0 +1,11 @@
+try {
+    console.log('Put de code');
+    const yeffer = 'a'
+    yeffer = 10
+} catch (error) {
+    console.log(`The mistake is ${error}`);
+}finally{
+
+    console.log('always iam here');
+
+}
