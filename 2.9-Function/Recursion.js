ce985aa46d586a64d0ea787bc55350efc7467151
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-03-10 22:31:50 -0500

    Update Recursion.js

--- 2.9-Function/Recursion.js
+++ 2.9-Function/Recursion.js
@@ -31,3 +31,29 @@ function fibonacci(n) {
 
 // Ejemplo de uso:
 console.log(fibonacci(7)); // Output: 13
+// Definición de un nodo de un árbol
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+}
+
+// Función recursiva para recorrer un árbol
+function traverseTree(node) {
+    console.log(node.value);
+    node.children.forEach(child => traverseTree(child));
+}
+
+// Ejemplo de uso:
+const rootNode = new TreeNode(1);
+const child1 = new TreeNode(2);
+const child2 = new TreeNode(3);
+const grandchild1 = new TreeNode(4);
+const grandchild2 = new TreeNode(5);
+
+rootNode.children.push(child1, child2);
+child1.children.push(grandchild1);
+child2.children.push(grandchild2);
+
+traverseTree(rootNode);
