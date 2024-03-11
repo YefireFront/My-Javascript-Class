42a07d8f7b96163a6635cfdb43081db9c758b720
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-03-10 22:30:58 -0500

    Update Recursion.js

--- 2.9-Function/Recursion.js
+++ 2.9-Function/Recursion.js
@@ -11,4 +11,13 @@
 
 
 
- 
+ function factorial(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+// Ejemplo de uso:
+console.log(factorial(5)); // Output: 120
