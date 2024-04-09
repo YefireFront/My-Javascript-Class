7183a5364db0a106c5f0fd12d130aec6ea05dcb2
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-04-09 10:42:01 -0500

    Update DAYtoDAY.js

--- DAYtoDAY.js
+++ DAYtoDAY.js
@@ -5,4 +5,6 @@ console.log(`April 4th a good effort `);
 console.log(`April 5th 💀 today was a wasted day in partre`);
 console.log(`April 6th `);
 console.log(`April 7th Iam creating a road and example for my first Lab Class`);
+console.log(`April 8th I STARED THE LAB WITH THE GUYS`);
+console.log(`April 7th Iam creating a road and example for my first Lab Class`);
 
