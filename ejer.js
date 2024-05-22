840e080714a170ffd9984ae388a7d1df0e3f844f
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-05-22 07:42:45 -0500

    Update ejer.js

--- ejer.js
+++ ejer.js
@@ -1,18 +1,2 @@
-let divi = [10 , 2, 52, 25, 8]
+let profesor = ' yeffer'
 
-let div = 0
-
-let fn = () =>{
-    for (let i = 0; i < divi.length; i++) {
-
-        div = divi[i] / divi[i + 1]
-        
-        if(divi[i - 1] == divi[i - 1]){
-            
-        }
-        console.log("el resultado es:" + div)
-    }
- 
-}
-
-fn()
