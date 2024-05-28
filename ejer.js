e56e2fcdbe55ee0f69dddfcb2da01da3a9b28ee4
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-05-28 18:22:34 -0500

    clAA poo

--- ejer.js
+++ ejer.js
@@ -1,134 +1,143 @@
-function outerFunction() {
-  var outerVariable = "Hello";
+// function outerFunction() {
+//   var outerVariable = "Hello";
 
-  function innerFunction() {
-    console.log(outerVariable);
-  }
+//   function innerFunction() {
+//     console.log(outerVariable);
+//   }
 
-  return innerFunction;
-}
+//   return innerFunction;
+// }
 
-var closureExample = outerFunction();
-closureExample(); // Output: Hello
+// var closureExample = outerFunction();
+// closureExample(); // Output: Hello
 
-function createBankAccount(initialBalance) {
-  var balance = initialBalance;
+// function createBankAccount(initialBalance) {
+//   var balance = initialBalance;
 
-  function deposit(amount) {
-    balance += amount;
-    console.log("Deposit:", amount);
-    console.log("New balance:", balance);
-  }
+//   function deposit(amount) {
+//     balance += amount;
+//     console.log("Deposit:", amount);
+//     console.log("New balance:", balance);
+//   }
 
-  function withdraw(amount) {
-    if (amount <= balance) {
-      balance -= amount;
-      console.log("Withdraw:", amount);
-      console.log("New balance:", balance);
-    } else {
-      console.log("Insufficient funds");
-    }
-  }
+//   function withdraw(amount) {
+//     if (amount <= balance) {
+//       balance -= amount;
+//       console.log("Withdraw:", amount);
+//       console.log("New balance:", balance);
+//     } else {
+//       console.log("Insufficient funds");
+//     }
+//   }
 
-  function getBalance() {
-    console.log("Current balance:", balance);
-  }
+//   function getBalance() {
+//     console.log("Current balance:", balance);
+//   }
 
-  return {
-    deposit: deposit,
-    withdraw: withdraw,
-    getBalance: getBalance,
-  };
-}
+//   return {
+//     deposit: deposit,
+//     withdraw: withdraw,
+//     getBalance: getBalance,
+//   };
+// }
 
-var account1 = createBankAccount(1000);
-account1.deposit(500);
-account1.withdraw(200);
-account1.getBalance();
+// var account1 = createBankAccount(1000);
+// account1.deposit(500);
+// account1.withdraw(200);
+// account1.getBalance();
 
-var account2 = createBankAccount(2000);
-account2.deposit(1000);
-account2.withdraw(5000);
-account2.getBalance();
+// var account2 = createBankAccount(2000);
+// account2.deposit(1000);
+// account2.withdraw(5000);
+// account2.getBalance();
 
-function fetchData() {
-  return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation
-    setTimeout(() => {
-      const data = "Data fetched successfully";
-      resolve(data);
-    }, 2000);
-  });
-}
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     // Simulating an asynchronous operation
+//     setTimeout(() => {
+//       const data = "Data fetched successfully";
+//       resolve(data);
+//     }, 2000);
+//   });
+// }
 
-fetchData()
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// fetchData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-async function fetchData() {
-  return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation
-    setTimeout(() => {
-      const data = "Data fetched successfully";
-      resolve(data);
-    }, 2000);
-  });
-}
+// async function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     // Simulating an asynchronous operation
+//     setTimeout(() => {
+//       const data = "Data fetched successfully";
+//       resolve(data);
+//     }, 2000);
+//   });
+// }
 
-async function example() {
-  try {
-    const data = await fetchData();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function example() {
+//   try {
+//     const data = await fetchData();
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-example();
+// example();
 
-function multiplyBy(factor) {
-  return function (number) {
-    return number * factor;
-  };
-}
+// function multiplyBy(factor) {
+//   return function (number) {
+//     return number * factor;
+//   };
+// }
 
-var multiplyByTwo = multiplyBy(2);
-console.log(multiplyByTwo(5)); // Output: 10
+// var multiplyByTwo = multiplyBy(2);
+// console.log(multiplyByTwo(5)); // Output: 10
 
-var multiplyByThree = multiplyBy(3);
-console.log(multiplyByThree(5)); // Output: 15
+// var multiplyByThree = multiplyBy(3);
+// console.log(multiplyByThree(5)); // Output: 15
 
-class FootballMatch {
-  constructor(homeTeam, awayTeam) {
-    this.homeTeam = homeTeam;
-    this.awayTeam = awayTeam;
-    this.homeScore = 0;
-    this.awayScore = 0;
-  }
+// class FootballMatch {
+//   constructor(homeTeam, awayTeam) {
+//     this.homeTeam = homeTeam;
+//     this.awayTeam = awayTeam;
+//     this.homeScore = 0;
+//     this.awayScore = 0;
+//   }
 
-  goal(team) {
-    if (team === this.homeTeam) {
-      this.homeScore++;
-    } else if (team === this.awayTeam) {
-      this.awayScore++;
-    } else {
-      console.log("Invalid team");
-    }
-  }
+//   goal(team) {
+//     if (team === this.homeTeam) {
+//       this.homeScore++;
+//     } else if (team === this.awayTeam) {
+//       this.awayScore++;
+//     } else {
+//       console.log("Invalid team");
+//     }
+//   }
 
-  getScore() {
-    console.log(
-      `${this.homeTeam} ${this.homeScore} - ${this.awayScore} ${this.awayTeam}`
-    );
-  }
-}
+//   getScore() {
+//     console.log(
+//       `${this.homeTeam} ${this.homeScore} - ${this.awayScore} ${this.awayTeam}`
+//     );
+//   }
+// }
 
-const match = new FootballMatch("Team A", "Team B");
-match.goal("Team A");
-match.goal("Team B");
-match.goal("Team A");
-match.getScore();
+// const match = new FootballMatch("Team A", "Team B");
+// match.goal("Team A");
+// match.goal("Team B");
+// match.goal("Team A");
+// match.getScore();
+
+
+let num = 52
+
+// Par
+// Impar
+//dividible 5 y 3
+
+
---
+++ repet.js
@@ -0,0 +1,35 @@
+// let numeros = [2, 4, 5, 2, 3, 5];
+
+// let numerosRepetidos = [];
+
+// for (let i = 0; i < numeros.length; i++) {
+//     let numeroActual = numeros[i];
+//     let contador = 0;
+
+//     for (let j = 0; j < numeros.length; j++) {
+//         if (numeroActual === numeros[j]) {
+//             contador++;
+//         }
+//     }
+
+//     if (contador > 1 ) {
+//         numerosRepetidos.push(numeroActual);
+//     }
+// }
+
+// console.log(numerosRepetidos); 
+
+
+ {
+    nombre: "John",
+    edad: 25,
+    equipo: null,
+    precio : 100
+    
+};
+
+
+console.log(jugador.nombre);
+console.log(jugador["edad"]);
+ 
+
