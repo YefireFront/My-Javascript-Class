ea6af09c8df2c556482eb9e8686be5ef256825d8
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-06-11 22:37:47 -0500

    Update ejer.js

--- ejer.js
+++ ejer.js
@@ -52,10 +52,7 @@ const jugador3 = new Jugador("Charlie", 20, 5);
 // Crear instancia de CasaDeApuestas
 const casaDeApuestas = new CasaDeApuestas();
 
-// Agregar jugadores a la casa de apuestas
-casaDeApuestas.agregarJugador(jugador1);
-casaDeApuestas.agregarJugador(jugador2);
-casaDeApuestas.agregarJugador(jugador3);
+
 
 // Iniciar el juego
 casaDeApuestas.iniciarJuego();
