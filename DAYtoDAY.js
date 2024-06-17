8dc7f0696c19b2b589536c0b9da7d47e0f0014e2
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-06-16 20:44:04 -0500

    Update DAYtoDAY.js

--- DAYtoDAY.js
+++ DAYtoDAY.js
@@ -72,6 +72,6 @@ console.log(`June  12th   Sell Monitr & sell Nev & buy Bet`)
 console.log(`June  13th   OJOOOOOO....`) 
 console.log(`June  14th   Figthing with my self`) 
 console.log(`June  15th   In the Alanas examn dopler, she is okay... CC birthday, i dont know whether to go, i can `) 
-console.log(`June  16th    `) 
+console.log(`June  16th   MY QUEEN BIRTH DAY `) 
 
 
