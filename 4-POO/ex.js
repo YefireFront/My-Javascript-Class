d23464b76d624ea9a2a3c30f38923860d8df8f49
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-06-28 10:35:33 -0500

    modify

--- 4-POO/ex.js
+++ 4-POO/ex.js
@@ -33,7 +33,7 @@ const libro = {
 };
 
 
-const personajeOnePiece = {
+const personaje1 = {
     nombre: "Monkey D. Luffy",
     edad: 19,
     tripulacion: "Piratas de Sombrero de Paja",
@@ -58,19 +58,127 @@ const personajeOnePiece = {
         { nombre: "Nami", rol: "Navegante" },
         { nombre: "Sanji", rol: "Cocinero" }
     ],
-    enemigos: [
-        { nombre: "Marshall D. Teach", alias: "Barbanegra", recompensa: "2,247,600,000 Berries" },
-        { nombre: "Kaido", alias: "Kaido de las Bestias", recompensa: "4,611,100,000 Berries" }
-    ],
-    aventuras: [
-        { nombre: "Saga de Arabasta", descripcion: "Luffy ayuda a la princesa Vivi a salvar su reino de Crocodile" },
-        { nombre: "Saga de Enies Lobby", descripcion: "Luffy y su tripulación enfrentan al CP9 para rescatar a Nico Robin" }
-    ],
-    redesSociales: {
-        twitter: null,
-        instagram: null,
-        facebook: null,
-        tiktok: "@LuffyThePirateKing"
-    },
     estadoActual: "Vivo"
 };
+
+const personaje2 = {
+    nombre: "Trafalgar D. Water Law",
+    edad: 26,
+    tripulacion: "Piratas de Heart",
+    origen: "North Blue",
+    recompensa: "500,000,000 Berries",
+    frutaDelDiablo: "Ope Ope no Mi",
+    habilidades: {
+        hakiObservacion: true,
+        hakiArmadura: true,
+        tecnicas: ["Shambles", "Room", "Gamma Knife"]
+    },
+    apariencia: {
+        altura: "191 cm",
+        peso: "78 kg",
+        cabello: "Negro",
+        ojos: "Ámbar",
+        vestimenta: "Abrigo largo, pantalones oscuros, sombrero de copa"
+    },
+    aliados: [
+        { nombre: "Bepo", rol: "Navegante" },
+        { nombre: "Jean Bart", rol: "Timonel" },
+        { nombre: "Penguin", rol: "Artillero" }
+    ],
+    estadoActual: "Vivo"
+};
+
+const personaje3 = {
+    nombre: "Charlotte Katakuri",
+    edad: 48,
+    tripulacion: "Piratas de Big Mom",
+    origen: "Whole Cake Island",
+    recompensa: "1,057,000,000 Berries",
+    frutaDelDiablo: "Mochi Mochi no Mi",
+    habilidades: {
+        hakiObservacion: true,
+        hakiArmadura: true,
+        tecnicas: ["Mochi Thrust", "Block Mochi", "Mochi Golem"]
+    },
+    apariencia: {
+        altura: "509 cm",
+        peso: "185 kg",
+        cabello: "Negro",
+        ojos: "Ámbar",
+        vestimenta: "Kimono negro, bufanda rosa, sombrero de copa"
+    },
+    aliados: [
+        { nombre: "Charlotte Smoothie", rol: "Comandante" },
+        { nombre: "Charlotte Cracker", rol: "Comandante" },
+        { nombre: "Charlotte Perospero", rol: "Comandante" }
+    ],
+    estadoActual: "Vivo"
+};
+
+const personaje4 = {
+    nombre: "Marshall D. Teach",
+    edad: 40,
+    tripulacion: "Piratas de Barbanegra",
+    origen: "Grand Line",
+    recompensa: "2,247,600,000 Berries",
+    frutaDelDiablo: "Yami Yami no Mi",
+    habilidades: {
+        hakiObservacion: true,
+        hakiArmadura: true,
+        tecnicas: ["Darkness", "Black Hole", "Dark Vortex"]
+    },
+    apariencia: {
+        altura: "344 cm",
+        peso: "380 kg",
+        cabello: "Negro",
+        ojos: "Negros",
+        vestimenta: "Abrigo largo, sombrero de ala ancha"
+    },
+    aliados: [
+        { nombre: "Shiliew", rol: "Comandante" },
+        { nombre: "Jesus Burgess", rol: "Comandante" },
+        { nombre: "Doc Q", rol: "Médico" }
+    ],
+    estadoActual: "Vivo"
+};
+
+const personaje5 = {
+    nombre: "Kaido",
+    edad: 50,
+    tripulacion: "Piratas de las Bestias",
+    origen: "Wano Country",
+    recompensa: "4,611,100,000 Berries",
+    frutaDelDiablo: "Uo Uo no Mi (Model: Seiryu)",
+    habilidades: {
+        hakiObservacion: true,
+        hakiArmadura: true,
+        tecnicas: ["Boro Breath", "Thunder Bagua", "Kaido's Thunder"]
+    },
+    apariencia: {
+        altura: "685 cm",
+        peso: "10,000 kg",
+        cabello: "Negro",
+        ojos: "Ámbar",
+        vestimenta: "Kimono rojo, hombreras con forma de cabeza de dragón"
+    },
+    aliados: [
+        { nombre: "King", rol: "Comandante" },
+        { nombre: "Queen", rol: "Comandante" },
+        { nombre: "Jack", rol: "Comandante" }
+    ],
+    estadoActual: "Vivo"
+};
+
+
+
+function convertRecompensaToNumber(personaje) {
+    const recompensaString = personaje.recompensa.replace(/,/g, "").replace(" Berries", "");
+    const recompensaNumber = parseFloat(recompensaString);
+    return recompensaNumber;
+}
+
+const recompensaPersonaje1 = convertRecompensaToNumber(personaje1);
+const recompensaPersonaje2 = convertRecompensaToNumber(personaje2);
+const recompensaPersonaje3 = convertRecompensaToNumber(personaje3);
+const recompensaPersonaje4 = convertRecompensaToNumber(personaje4);
+const recompensaPersonaje5 = convertRecompensaToNumber(personaje5);
--- DAYtoDAY.js
+++ DAYtoDAY.js
@@ -1,62 +1,62 @@
-console.log(`April 1th   Reorganizacion `);
-console.log(`April 2th   Two it's was a good day with the studen in MT`);
-console.log(`April 3th   first _DataBase class with 11º`);
-console.log(`April 4th   a good effort `);
-console.log(`April 5th   💀 today was a wasted day in partre`);
-console.log(`April 6th   💀 forget the day`);
-console.log(`April 7th   Iam creating a road and example for my first Lab Class`);
-console.log(`April 8th   I STARED THE LAB WITH THE GUYS`);
-console.log(`April 9th   It was a simple day and i rowked un the pc room`);
-console.log(`April 10th  Wonderfull satabase class with the MT 11º `);
-console.log(`April 11th  we save the oportunity the change day to day`);
-console.log(`April 12th  I consfiused abou take the sound`);
-console.log(`April 13th  i didnt feel confitbale, cus  the thesicion was in my head`);
-console.log(`April 14th  it was an interesting day, trying to reorganize my routine.s `);
-console.log(`April 15th  booring day in anormal journary in the collage `);
-console.log(`April 16th  it was a good day, I was very strict with my students `);
-console.log(`April 17th  it was a grate day with my students practice SQL for the first time  `);
-console.log(`April 18th  I was working inn the journary `);
-console.log(`April 19th  I felt a good ambiance at school. `);
-console.log(`April 20th  I Lost the day`);
-console.log(`April 21th  this Sunday was a restart `);
-console.log(`April 22th  Today was the firts Lab Class, and was intersting`);
-console.log(`April 23th  The first time all MT 10th graders come to class`);
-console.log(`April 24th  Speach for MT10`);
-console.log(`April 25th  today I was at home doing school homework and did not concentrate on studying well. `);
-console.log(`April 26th  probelma with the schedule, problem with the computer rooms `);
-console.log(`April 27th  💀 `);
-console.log(`April 28th  STAR THE REST OF MY BIG CHANCE`);
-console.log(`April 29th  Today  i showed my lab students all my  `);
-console.log(`April 30th  Good class with my  MT 10º studens , with the pet visist  `);
-console.log(`April 1th   today was a holiday and I was working on organizing some things for the study plan. `);
-console.log(`April 2th   Thurdayyy... basci day , but in the end on the day i was workin in the medellin digital `);
-console.log(`April 3th   friday, simple day `);
-console.log(`April 4th   saturday, i was watching PG, but before i study DOM manipulation `);
-console.log(`April 5th   Wear day , but iam ready to start my change `);
-console.log(`April 6th   In the lab today practice array method and a bit DOM`);
-console.log(`April 7th   i talked seriusly with the MT 10º, bu a feel wear`);
-console.log(`April 8th   it was a good day, champions day, but we worked in BD suceccfully`);
-console.log(`April 9th   Lose this day`);
-console.log(`April 10th   talk eith the rector aboyut Programin festival and star my vacation`);
-console.log(`April 11th   simple day a watch PG`);
-console.log(`April 12th   finished PG`);
-console.log(`April 13th   holly day and did nothing special`);
-console.log(`April 14th   otganize my routine a practice a little bit`);
-console.log(`April 15th   teachers day. i bought some thin for the house, and practice My SQL`);
-console.log(`April 16th   i talked with myself and feel good `);
-console.log(`April 17th   just 3 pull in the bar 😭`);
-console.log(`April 18th   Good day win UI and Valeris Project 😭`);
-console.log(`April 19th   Hard Programing Practice`);
-console.log(`April 21th   🌧 crazy inn the mornig. now i gonna practice recat. FAILED `);
-console.log(`April 22th   Wera day , MT 11º no fue completo y raro `);
-console.log(`April 23th   DOFUS trying to back to my live  🌧`);
-console.log(`April 24th   stay at home `);
-console.log(`April 25th   🍻`);
-console.log(`April 26th   pos party day and relax`);
-console.log(`April 27th   Im battling with my self`);
-console.log(`April 28th   MY DAD is here`);
-console.log(`April 29th   Battle with Dofus`);
-console.log(`April 30th   Simple day..  stay dofus. i hope that the last dance`);
+console.log(`April 1th    Reorganizacion `);
+console.log(`April 2th    Two it's was a good day with the studen in MT`);
+console.log(`April 3th    first _DataBase class with 11º`);
+console.log(`April 4th    a good effort `);
+console.log(`April 5th    💀 today was a wasted day in partre`);
+console.log(`April 6th    💀 forget the day`);
+console.log(`April 7th    Iam creating a road and example for my first Lab Class`);
+console.log(`April 8th    I STARED THE LAB WITH THE GUYS`);
+console.log(`April 9th    It was a simple day and i rowked un the pc room`);
+console.log(`April 10th   Wonderfull satabase class with the MT 11º `);
+console.log(`April 11th   we save the oportunity the change day to day`);
+console.log(`April 12th   I consfiused abou take the sound`);
+console.log(`April 13th   i didnt feel confitbale, cus  the thesicion was in my head`);
+console.log(`April 14th   it was an interesting day, trying to reorganize my routine.s `);
+console.log(`April 15th   booring day in anormal journary in the collage `);
+console.log(`April 16th   it was a good day, I was very strict with my students `);
+console.log(`April 17th   it was a grate day with my students practice SQL for the first time  `);
+console.log(`April 18th   I was working inn the journary `);
+console.log(`April 19th   I felt a good ambiance at school. `);
+console.log(`April 20th   I Lost the day`);
+console.log(`April 21th   this Sunday was a restart `);
+console.log(`April 22th   Today was the firts Lab Class, and was intersting`);
+console.log(`April 23th   The first time all MT 10th graders come to class`);
+console.log(`April 24th   Speach for MT10`);
+console.log(`April 25th   today I was at home doing school homework and did not concentrate on studying well. `);
+console.log(`April 26th   probelma with the schedule, problem with the computer rooms `);
+console.log(`April 27th   💀 `);
+console.log(`April 28th   STAR THE REST OF MY BIG CHANCE`);
+console.log(`April 29th   Today  i showed my lab students all my  `);
+console.log(`April 30th   Good class with my  MT 10º studens , with the pet visist  `);
+console.log(`May   1th    today was a holiday and I was working on organizing some things for the study plan. `);
+console.log(`May   2th    Thurdayyy... basci day , but in the end on the day i was workin in the medellin digital `);
+console.log(`May   3th    friday, simple day `);
+console.log(`May   4th    saturday, i was watching PG, but before i study DOM manipulation `);
+console.log(`May   5th    Wear day , but iam ready to start my change `);
+console.log(`May   6th    In the lab today practice array method and a bit DOM`);
+console.log(`May   7th    i talked seriusly with the MT 10º, bu a feel wear`);
+console.log(`May   8th    it was a good day, champions day, but we worked in BD suceccfully`);
+console.log(`May   9th    Lose this day`);
+console.log(`May   10th   talk eith the rector aboyut Programin festival and star my vacation`);
+console.log(`May   11th   simple day a watch PG`);
+console.log(`May   12th   finished PG`);
+console.log(`May   13th   holly day and did nothing special`);
+console.log(`May   14th   otganize my routine a practice a little bit`);
+console.log(`May   15th   teachers day. i bought some thin for the house, and practice My SQL`);
+console.log(`May   16th   i talked with myself and feel good `);
+console.log(`May   17th   just 3 pull in the bar 😭`);
+console.log(`May   18th   Good day win UI and Valeris Project 😭`);
+console.log(`May   19th   Hard Programing Practice`);
+console.log(`May   21th   🌧 crazy inn the mornig. now i gonna practice recat. FAILED `);
+console.log(`May   22th   Wera day , MT 11º no fue completo y raro `);
+console.log(`May   23th   DOFUS trying to back to my live  🌧`);
+console.log(`May   24th   stay at home `);
+console.log(`May   25th   🍻`);
+console.log(`May   26th   pos party day and relax`);
+console.log(`May   27th   Im battling with my self`);
+console.log(`May   28th   MY DAD is here`);
+console.log(`May   29th   Battle with Dofus`);
+console.log(`May   30th   Simple day..  stay dofus. i hope that the last dance`);
 console.log(`June  01th   it june WTF... RESET DAY`)
 console.log(`June  02th   ☠`)
 console.log(`June  03th   Really Sick`)
@@ -81,7 +81,8 @@ console.log(`June  21th   DOFUS I REALIZE a ispend 305K in this shit `)
 console.log(`June  22th   Wonderfull Day with ALANA Paint and My maind Shake`) 
 console.log(`June  23th   Desicive Day `) 
 console.log(`June  24th   👑 It's Over. Show me win `) 
-console.log(`June  25th    I Read again`) 
-console.log(`June  26th    Reorganize my life`) 
+console.log(`June  25th   I Read again`) 
+console.log(`June  26th   Reorganize my life`) 
+console.log(`June  27th   S `) 
 
 
