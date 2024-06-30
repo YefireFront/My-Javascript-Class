b60ebe93ccfd522e7ad0480f2c61b6ac0fffb874
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-06-30 13:12:08 -0500

    Update DAYtoDAY.js

--- DAYtoDAY.js
+++ DAYtoDAY.js
@@ -86,6 +86,6 @@ console.log(`June  26th   Reorganize my life`)
 console.log(`June  27th   I went to bed late talking to ka`) 
 console.log(`June  28th   Important figthing day `) 
 console.log(`June  29th   Thinking day`) 
-console.log(`June  29th   THE LAST DANCE`) 
+console.log(`June  30th   THE LAST DANCE`) 
 
 
