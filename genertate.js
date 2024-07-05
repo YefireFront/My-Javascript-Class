ac6c730a716e69725fefcff5c615b4904f22fcfd
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-04 22:52:47 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -9,3 +9,20 @@ class People {
     }
 }
 
+class Technology {
+    name;
+    location;
+
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    displayLocation() {
+        console.log(`The ${this.name} is located at ${this.location}`);
+    }
+}
+
+// Example usage
+const laptop = new Technology('Laptop', 'Room 101');
+laptop.displayLocation();
