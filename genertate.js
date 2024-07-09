0e5b7298caad3100e196ed356b331a4b7eb5d7b8
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-07-09 12:41:41 -0500

    upd

---
+++ genertate.js
@@ -0,0 +1,30 @@
+
+let variable1 = 13;
+
+let variable2 = 13;
+
+let suma = variable1 + variable2;
+
+console.log(suma)
+
+
+if ( variable1 == variable2 ) {
+
+    console.log(variable1, "es igual a ", variable2);
+}
+
+
+
+
+
+
+
+
+if ( variable1 > variable2 ) {
+
+    console.log(variable1, "es mayor que ", variable2);
+    
+}else{
+
+    console.log(variable2, "es mayor que ", variable1);
+}
