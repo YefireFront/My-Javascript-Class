683377e53378d3893c1d390179f5b2ba19ae86a7
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-24 22:17:08 -0500

    Create module.js

---
+++ 13 - Moduls/module.js
@@ -0,0 +1,16 @@
+// module.js
+
+// Función privada del módulo
+function privateFunction() {
+    // Código de la función privada
+}
+
+// Función pública del módulo
+function publicFunction() {
+    // Código de la función pública
+}
+
+// Exporta las funciones públicas del módulo
+module.exports = {
+    publicFunction
+};
