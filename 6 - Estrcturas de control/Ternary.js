f9cba03def49418632abc6bf97eb8ed861fdda81
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-26 15:12:58 -0500

    s crontrol

---
+++ 6 - Estrcturas de control/Ternary.js
@@ -0,0 +1,3 @@
+const edad = 18;
+const mensaje = edad >= 18 ? "Eres un adulto." : "Eres menor de edad.";
+console.log(mensaje); // Eres un adulto.
---
+++ 6 - Estrcturas de control/condicionales.js
@@ -0,0 +1,10 @@
+
+const edad = 18;
+
+if (edad < 18) {
+  console.log("Eres menor de edad.");
+} else if (edad >= 18 && edad < 65) {
+  console.log("Eres un adulto.");
+} else {
+  console.log("Eres una persona mayor.");
+}
