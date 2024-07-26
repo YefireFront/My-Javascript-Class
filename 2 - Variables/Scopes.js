82a6c4947467015d89a11f0a61f6c0526f39e855
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-26 14:35:21 -0500

    Create Scopes.js

---
+++ 2 - Variables/Scopes.js
@@ -0,0 +1,17 @@
+// Global Scope
+
+var globalVar = "I am global";
+let globalLet = "I am also global";
+const globalConst = "I am globally constant";
+
+function checkGlobalScope() {
+  console.log(globalVar);   // I am global
+  console.log(globalLet);   // I am also global
+  console.log(globalConst); // I am globally constant
+}
+
+checkGlobalScope();
+
+console.log(globalVar);   // I am global
+console.log(globalLet);   // I am also global
+console.log(globalConst); // I am globally constant
