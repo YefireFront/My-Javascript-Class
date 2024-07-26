931f17681d683cf22473edac1beac195a55c3aed
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-26 15:07:16 -0500

    Update Scopes.js

--- 2 - Variables/Scopes.js
+++ 2 - Variables/Scopes.js
@@ -15,3 +15,59 @@ checkGlobalScope();
 console.log(globalVar);   // I am global
 console.log(globalLet);   // I am also global
 console.log(globalConst); // I am globally constant
+
+
+
+
+
+// . Function Scope
+
+function checkFunctionScope() {
+    var functionVar = "I am in a function";
+    let functionLet = "I am also in a function";
+    const functionConst = "I am constant in a function";
+    
+    console.log(functionVar);   // I am in a function
+    console.log(functionLet);   // I am also in a function
+    console.log(functionConst); // I am constant in a function
+  }
+  
+  checkFunctionScope();
+  
+  // console.log(functionVar); // Error: functionVar is not defined
+  // console.log(functionLet); // Error: functionLet is not defined
+  // console.log(functionConst); // Error: functionConst is not defined
+  
+
+
+
+
+//   Block Scope
+
+
+if (true) {
+    var blockVar = "I am a var inside a block";
+    let blockLet = "I am a let inside a block";
+    const blockConst = "I am a const inside a block";
+    
+    console.log(blockVar);   // I am a var inside a block
+    console.log(blockLet);   // I am a let inside a block
+    console.log(blockConst); // I am a const inside a block
+  }
+  
+  console.log(blockVar); // I am a var inside a block
+  // console.log(blockLet); // Error: blockLet is not defined
+  // console.log(blockConst); // Error: blockConst is not defined
+  
+
+
+
+
+//   Module Scope
+
+
+export const moduleVar = "I am in a module";
+
+
+import { moduleVar } from './module1.js';
+console.log(moduleVar); // I am in a module
