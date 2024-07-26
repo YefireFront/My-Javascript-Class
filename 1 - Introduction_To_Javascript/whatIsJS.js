b2976ac358df34ea918db4ce39bdbe6ee6ade074
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-07-26 14:27:12 -0500

    Create whatIsJS.js

---
+++ 1 - Introduction_To_Javascript/whatIsJS.js
@@ -0,0 +1,8 @@
+// JavaScript is a high-level, interpreted programming language that is widely used for web development. It is a versatile language that allows developers to create dynamic and interactive content on web pages. JavaScript can run in the browser, allowing for client-side interactivity, and on the server side using environments like Node.js.
+
+// Key features of JavaScript:
+
+// Event-driven: JavaScript can respond to user actions such as clicks, form submissions, and more.
+// Object-oriented: JavaScript supports object-oriented programming, allowing for the creation of objects and classes.
+// Dynamic typing: Variables in JavaScript do not require a declared type and can change type dynamically.
+// Prototype-based: In JavaScript, inheritance is achieved through prototypes rather than classical inheritance models.
