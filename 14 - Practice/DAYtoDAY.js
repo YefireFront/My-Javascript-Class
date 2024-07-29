59b7900b2bb03359e7ca5a7cc7bc8599aa59f8ed
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-07-28 21:52:46 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -112,7 +112,7 @@ console.log(`July  22th     i ll back to the school great class with the lab`)
 console.log(`July  23th     Great exercise with the 10º  takl about the the teacher`) 
 console.log(`July  24th     advising my group with the projects`) 
 console.log(`July  25th     simple day in the room watching green feet `) 
-console.log(`July  26th      `) 
+console.log(`July  26th      Waiting`) 
 console.log(`July  27th      `) 
 console.log(`July  28th      ALANA`) 
 
