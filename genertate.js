912e4e8914d90cb66436a5a48c5eda61ef0f6aa0
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-07-30 12:46:03 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -29,3 +29,16 @@ alert("Tu puntuación final es: " + puntos);
 
 
 
+  // Accumulator example
+  let numbers = [1, 2, 3, 4, 5];
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  console.log("The sum of the numbers is: " + sum);
+
+
+
+
