2440127e576e234d76cd45e678a3289e97300a68
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-07-31 12:43:08 -0500

    upd

--- genertate.js
+++ genertate.js
@@ -25,19 +25,8 @@ if (respuesta4 === '2') {
 }
 
 // Mostrar resultado final
-alert("Tu puntuación final es: " + puntos);
+console.log("Tu puntuación final es: " + puntos);
 
-
-
-  // Accumulator example
-  let numbers = [1, 2, 3, 4, 5];
-  let sum = 0;
-
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-
-  console.log("The sum of the numbers is: " + sum);
 
 
 
