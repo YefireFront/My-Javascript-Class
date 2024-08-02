071b8b5e96a13697f56c7fc0868bbc37f7862b8b
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-08-01 23:45:51 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -118,6 +118,6 @@ console.log(`July  28th      ALANA 💘`)
 console.log(`July  29th      love at first sight A ❤`) 
 console.log(`July  30th      `) 
 console.log(`July  31th      `) 
-console.log(`Agos  01th      `) 
+console.log(`Agos  01th      Lab 10 Amagzing class`) 
 
 
