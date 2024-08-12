ffe457390705ead858ccb8e7a35f40e12f2b3082
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-08-11 22:49:06 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -1,12 +1,5 @@
 // Example 1: Using Promises
 
-// Function that returns a Promise
-function fetchData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Data fetched successfully!');
-        }, 2000);
-    });
-}
+
 
 
