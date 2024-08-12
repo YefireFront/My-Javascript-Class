69c47ec5e527f59b245b5d2d438a121481cd3a00
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-08-11 22:50:47 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -128,6 +128,6 @@ console.log(`Agos  07th     HOLLY DAY`)
 console.log(`Agos  08th     Vaccination day for the queen`) 
 console.log(`Agos  09th     Trying to fix the computer `) 
 console.log(`Agos  10th     i was with Alana`) 
-console.log(`Agos  11th      `) 
+console.log(`Agos  11th     In the process to fix me`) 
 
 
