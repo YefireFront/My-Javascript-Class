15b62740674f9be031839d7530fd4eb72ae0f59e
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-08-21 16:43:49 -0500

    upd

---
+++ pass.js
@@ -0,0 +1,23 @@
+const yeffer = {
+    name: 'Yeffer',
+    age: 24,
+    country: 'Colombia',
+    
+}
+const Quintero = {
+    name: 'Quintero',
+    age: 28,
+    country: 'Colombia',
+}
+const Lebron = {
+    name: 'Lebron',
+    age: 36,
+    country: 'USA',
+}
+
+const kobe = {
+    name: 'Kobe',
+    age: 41,
+    country: 'USA',
+}
+
