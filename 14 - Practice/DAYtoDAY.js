010ee176ba44da70f768a00699f32ee47b7996bd
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-09-06 22:42:42 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -152,5 +152,6 @@ console.log(`Agos  30th     DAY 19`)
 console.log(`Agos  31th     DAY 20`) 
 console.log(`Sept  01th     ⌛`) 
 console.log(`Sept  02th     `) 
+console.log(`Sept  02th     `) 
 
 
