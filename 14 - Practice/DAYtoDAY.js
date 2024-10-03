49e7fa5548eecbb38549ce86ceae0f06abf52bfb
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-10-02 22:37:06 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -181,5 +181,6 @@ console.log(`Sept  28th     RETAKIN REATC `)
 console.log(`Sept  29th     talking with ITM teacher about the MT`)
 console.log(`Sept  30th     Star to teach the VidoGame build`)
 console.log(`Sept  01th     All 9ª Reunion`)
+console.log(`Sept  02th     `)
 
 
