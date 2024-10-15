5d30b6e75cadd0abc2bc1effab762ef727477107
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-10-14 20:35:28 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -194,7 +194,7 @@ console.log(`Octo  11th     Went to the dentist 🦷`)
 console.log(`Octo  12th     `)
 console.log(`Octo  13th     Dragon Ball Z sparking zero `)
 console.log(`Octo  14th     I FEEL its the time`)
-console.log(`Octo  15th     `)
+console.log(`Octo  15th     ALL SATRT 🧇`)
  
 
 
