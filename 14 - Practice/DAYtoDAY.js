31070196f2c350953371ecba4f422cec0d8fefa6
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-11-07 23:01:30 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -217,6 +217,7 @@ console.log(`Octo  03th     🍹🍹`)
 console.log(`Octo  04th     Back home a rest`)
 console.log(`Octo  05th     MT 10 Final Maquet exam `)
 console.log(`Octo  06th     evoid the school PARO`)
+console.log(`Octo  06th     evoid the school PARO`)
  
 
 
