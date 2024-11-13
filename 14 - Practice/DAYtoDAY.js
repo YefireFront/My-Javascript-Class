0367decc7418cfcf8162d41a4c9389d83135ca1f
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-11-12 22:27:33 -0500

    Update DAYtoDAY.js

--- 14 - Practice/DAYtoDAY.js
+++ 14 - Practice/DAYtoDAY.js
@@ -222,7 +222,7 @@ console.log(`Octo  08th     `)
 console.log(`Octo  09th     🍹`)
 console.log(`Octo  10th     🍹`)
 console.log(`Octo  11th     Reset my mind`)
-console.log(`Octo  12th     THE OPORTUNITY OF MY LIFE, ALL CAN CHANGE I NEED YOUN GOD 🙏`)
+console.log(`Octo  12th     THE OPORTUNITY OF MY LIFE, ALL CAN CHANGE I NEED YOUN GOD 🙏, PLEASE DONT FORGET ME 💙`)
  
 
 
