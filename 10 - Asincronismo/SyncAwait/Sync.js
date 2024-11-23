d4ea723a9544e8778dc9536dc3b965d5ade7d4b5
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-11-22 23:14:26 -0500

    Update Sync.js

--- 10 - Asincronismo/SyncAwait/Sync.js
+++ 10 - Asincronismo/SyncAwait/Sync.js
@@ -38,3 +38,20 @@ async function fetchData() {
 }
 
 fetchData();
+
+
+
+async function fetchPokemon(pokemonName) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('Fetched Pokemon Data:', data);
+  } catch (error) {
+    console.error('Error fetching Pokemon data:', error);
+  }
+}
+
+fetchPokemon('pikachu');
