4ac76540602f9df0c32d4dbb2ce4a92188f344bc
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-11-23 23:21:52 -0500

    Create set.js

---
+++ 3 - Tipos de Datos/no-Primitivos/5-Sets/set.js
@@ -0,0 +1,15 @@
+// Crear un Set con 5 nombres de jugadores
+const playersSet = new Set(['Lionel', 'Cristiano', 'Kylian', 'Neymar', 'Kevin']);
+
+// Crear un array con 10 nombres de jugadores, incluyendo 3 nombres repetidos
+const playersArray = ['Lionel', 'Cristiano', 'Kylian', 'Neymar', 'Kevin', 'Lionel', 'Cristiano', 'Kevin', 'Sadio', 'Eden'];
+
+playersSet.forEach(player => console.log(player));
+
+// playersArray.forEach(player => console.log(player));
+
+
+const playerunique = [... new Set (playersArray)]
+
+console.log(playerunique)
+
