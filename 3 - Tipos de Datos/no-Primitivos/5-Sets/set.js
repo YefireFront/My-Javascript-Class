9069af007a9ccdaaf64b147b307810a0f5b8cbbd
Yeffer Moreno Robledo <yefire.js@gmail.com>
2024-12-09 23:55:22 -0500

    Update set.js

--- 3 - Tipos de Datos/no-Primitivos/5-Sets/set.js
+++ 3 - Tipos de Datos/no-Primitivos/5-Sets/set.js
@@ -36,3 +36,12 @@ languagesSet.forEach(language => console.log(language));
 
 // Obtener el tamaño del Set
 console.log(`Número de lenguajes en el Set: ${languagesSet.size}`);
+
+console.log(languagesSet.has('Java')); // true
+console.log(languagesSet.has('C++')); // false
+
+// Recorrer y mostrar los elementos del Set
+languagesSet.forEach(language => console.log(language));
+
+// Obtener el tamaño del Set
+console.log(`Número de lenguajes en el Set: ${languagesSet.size}`);
