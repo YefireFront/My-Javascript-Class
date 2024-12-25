320aa6d0c1f1b5aeceb2fefcde2041096e562ea4
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2024-12-24 23:31:53 -0500

    Update pass.js

--- pass.js
+++ pass.js
@@ -1,3 +1,9 @@
 const gestorTurnos = {
+    jugadores: ['Jugador 1', 'Jugador 2', 'Jugador 3'],
+    indiceActual: 0,
+
+    siguienteTurno() {
+        this.indiceActual = this.indiceActual === this.jugadores.length - 1 ? 0 : this.indiceActual + 1;
+        con
 
 
