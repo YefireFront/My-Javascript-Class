e8141c8ad357476615bd959417bdf0bfbe5995b0
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-01-11 23:39:25 -0500

    Update switch.js

--- 6 - Estrcturas de control/switch.js
+++ 6 - Estrcturas de control/switch.js
@@ -15,15 +15,3 @@ switch (fruta) {
 }
 
 
-// switch (fruta) {
-//   case 'naranja':
-//     console.log('La naranja es deliciosa.');
-//     break;
-//   case 'manzana':
-//     console.log('La manzana es crujiente.');
-//     break;
-//   case 'plátano':
-//     console.log('El plátano es suave.');
-//     break;
-//   default:
-//     console.log('No tengo información sobre esa fruta.');    
