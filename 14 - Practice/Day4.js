8dee0f1a59fa81978268a5bbb7e4d1398dd104f4
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-01-25 21:51:52 -0500

    Update Day4.js

--- 14 - Practice/Day4.js
+++ 14 - Practice/Day4.js
@@ -18,23 +18,3 @@ const uniqueRoadmaps3 = roadmaps.reduce((unique, roadmap) => {
 }, []);
 console.log(uniqueRoadmaps3);
 
-//*4
-
-const uniqueRoadmaps4 = [];
-roadmaps.forEach((roadmap) => {
-  if (!uniqueRoadmaps4.includes(roadmap)) {
-    uniqueRoadmaps4.push(roadmap);
-  }
-});
-console.log(uniqueRoadmaps4);
-
-//*5
-
-const uniqueRoadmap5s = [];
-for (const roadmap of roadmaps) {
-  if (!uniqueRoadmap5s.includes(roadmap)) {
-    uniqueRoadmap5s.push(roadmap);
-  }
-}
-console.log(uniqueRoadmap5s);
-console.log(uniqueRoadmap5s);
