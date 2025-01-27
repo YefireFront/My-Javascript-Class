1496fc855ab0f600caff738bff49a52f49268efc
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-01-26 22:38:19 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -5,3 +5,12 @@ setTimeout(() => {
 }, 3000);
 
 console.log("Finalizado.");
+
+function cambiarFondo() {
+    document.body.style.backgroundColor = 'lightblue';
+    console.log("El color de fondo ha cambiado a azul claro.");
+}
+
+console.log("Espera 5 segundos para cambiar el fondo...");
+
+setTimeout(cambiarFondo, 5000);
