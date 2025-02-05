e14fc04ed5693be1dede8a36e6fc6510c5a869ff
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-02-04 21:35:57 -0500

    Update set.js

--- 3 - Tipos de Datos/no-Primitivos/5-Sets/set.js
+++ 3 - Tipos de Datos/no-Primitivos/5-Sets/set.js
@@ -16,4 +16,23 @@ console.log(playerunique)
 
 
 
-/
+// Crear un Set con algunos lenguajes de programación
+const languagesSet = new Set(['JavaScript', 'Python', 'Java', 'C++', 'Ruby']);
+
+// Añadir un nuevo lenguaje al Set
+languagesSet.add('Go');
+
+// Intentar añadir un lenguaje duplicado no tendrá efecto
+languagesSet.add('Python');
+
+// Eliminar un lenguaje del Set
+languagesSet.delete('C++');
+
+// Comprobar si un lenguaje está en el Set
+console.log(languagesSet.has('Java')); // true
+console.log(languagesSet.has('C++')); // false
+
+// Convertir el Set a un Array
+const languagesArray = [...languagesSet];
+
+console.log(languagesArray); // ['JavaScript', 'Python', 'Java', 'Ruby', 'Go']
