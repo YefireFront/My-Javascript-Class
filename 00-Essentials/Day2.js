d904133b2ef0f41cd73d5ab4fa074d422fb9e6f5
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-06-04 23:26:51 -0500

    Update Day2.js

--- 00-Essentials/Day2.js
+++ 00-Essentials/Day2.js
@@ -1,4 +1,4 @@
-console.log(`01 Jun  01th Drinking`)
-console.log(`02 Jun  02th Drinking`)
-console.log(`03 Jun  03th Final Day`)
-console.log(`04 Jun  03th Meeting the posible disaster coor`)
+console.log(`Jun 01th Drinking`)
+console.log(`Jun 02th Drinking`)
+console.log(`Jun 03th Final Day`)
+console.log(`Jun 04th Meeting the posible disaster coor`)
