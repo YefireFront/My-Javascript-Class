ac3f2ebb36811585df1fa8547f022b5723f103ad
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-07-04 23:14:28 -0500

    Update 01-Data.js

--- 00-Essentials/01-Data.js
+++ 00-Essentials/01-Data.js
@@ -1,16 +1,1 @@
-// Primitivos
-console.log(typeof 'Bayer Munich')
-console.log(typeof 15)
-console.log(typeof true)
-
-console.log(typeof undefined)
-console.log(typeof null)
-
-
-// No Primitivos
-console.log(typeof {})
-console.log(typeof [])
-
-
-
 
