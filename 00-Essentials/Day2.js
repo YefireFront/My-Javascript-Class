2055abfdb69a35ef82c0c39652397b197154c8a5
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-07-24 22:57:27 -0500

    Update Day2.js

--- 00-Essentials/Day2.js
+++ 00-Essentials/Day2.js
@@ -52,7 +52,7 @@ console.log(`Jul 21th My operation 👁`)
 console.log(`Jul 21th  TDEA`)
 console.log(`Jul 22th  Cita`)   
 console.log(`Jul 23th  Stay at home`)   
-console.log(`Jul 24th  General Aful exam`)   
+console.log(`Jul 24th  General Aful exam `)   
 
 
 
