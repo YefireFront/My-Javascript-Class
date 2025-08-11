07394f162af67447af28aeaac0b31c4794fc3b52
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-08-10 23:20:44 -0500

    Update API.js

--- Practice/API.js
+++ Practice/API.js
@@ -34,7 +34,6 @@ async function obtenerUsuariosAsync() {
     }
 }
     
-    
 function getPostsbyID(id) {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res=>{
@@ -81,7 +80,6 @@ async function getPostByID(id) {
     
 }
 
-
 function postsUser(idUser) {
     fetch(`https://jsonplaceholder.typicode.com/users/${idUser}`)
     .then(res =>{
@@ -113,7 +111,6 @@ function postsUser(idUser) {
         console.log(`hubo un error en la peticion ${error}`)
     })
 }
-
 
 
 async function asyncPostsUser(userID) {
@@ -151,8 +148,6 @@ function getMultipleUsers([user1, user2, user3]) {
 
 }
 
-
-
 // getMultipleUsers([2, 4, 6])
 
 
@@ -163,4 +158,57 @@ async function obtenerUsuarios([user1, user2, user3]) {
         fetch(`https://jsonplaceholder.typicode.com/users/${user3}`),
     ])
 
+    const [usu1, usu2, usu3] =  await Promise.all([res1.json(), res2.json(), res3.json()])
+    console.log(`${usu1.name}`)
+
+
 }
+
+// obtenerUsuarios([1,2,3])
+
+
+const BASE_URL = 'https://rickandmortyapi.com/api';
+
+const get = (url) => fetch(url).then((response)=> response.json())
+
+async function getAllCharacters(episode) {
+
+    const {characters : charactersUrls} = await get(`${BASE_URL}/episode/${episode}`)
+
+    const charactersPromises = await charactersUrls.map(charactersUrls => get(charactersUrls))
+
+    const data = await Promise.all(charactersPromises)
+
+    console.log(data)
+
+    data.forEach(personaje =>{
+        console.log(`Personaje:${personaje.name}\nEspecie: ${personaje.species}\nGenero: ${personaje.gender}`)
+    })
+
+    
+}
+
+
+// getAllCharacters(4)
+
+
+
+const BASE_URL_POKEMON = 'https://pokeapi.co/api/v2/pokemon?limit=30';
+
+const fetchJson = (url) => fetch(url).then(response => response.json());
+
+async function getPokemonList() {
+    const { results } = await fetchJson(BASE_URL_POKEMON);
+
+    const pokemonPromises = results.map(pokemon => fetchJson(pokemon.url));
+
+    const pokemonData = await Promise.all(pokemonPromises);
+
+    pokemonData.forEach(poke =>{
+        console.log(poke.name)
+    })
+}
+
+getPokemonList();
+
+
