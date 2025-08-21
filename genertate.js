e9094bee0a3367526ef1c26e352ec9a9280d11aa
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-08-20 22:30:35 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -3,11 +3,11 @@
 const data = fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
 .then(res => res.json())
 .then(res =>  {
-    // res.data.forEach(element => {
-    //     console.log(element)
-    // });
+    res.data.forEach(element => {
+        console.log(element)
+    });
 
-    // console.log(res.data[0])
+    console.log(res.data[0])
 })
 
 
