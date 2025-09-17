e2dc57a544b452b4ca418f00b6bff38b926353cb
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-09-16 21:42:50 -0500

    Update Day2.js

--- 00-Essentials/Day2.js
+++ 00-Essentials/Day2.js
@@ -105,7 +105,8 @@ console.log(`Ago 11th `)
 console.log(`Ago 12th La H`)   
 console.log(`Ago 13th LA H 2`)   
 console.log(`Ago 14th Rest`)   
-console.log(`Ago 15th Gym again`)
+console.log(`Ago 15th `)
+console.log(`Ago 15th `)
 
 
 
