86e5b7702e4d37d612b31e1b5b4831853b20071c
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-09-22 21:42:08 -0500

    Update Day2.js

--- 00-Essentials/Day2.js
+++ 00-Essentials/Day2.js
@@ -111,7 +111,7 @@ console.log(`Ago 17th 10 class regular`)
 console.log(`Ago 18th Give up with 10`)
 console.log(`Ago 19th UPB reunion`)
 console.log(`Ago 20th Organize house the raskashin is back`)
-console.log(`Ago 21th NEW ATTEMPT `)
+console.log(`Ago 21th NEW ATTEMPT`)
 console.log(`Ago 22th Create SIEE`)
 
 
