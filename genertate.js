967019373bdb43e3de305011ecc8d84397a93988
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-09-26 23:23:07 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -8,24 +8,3 @@ class JugadorFutbol {
 }
 
 
-// Clase Equipo
-class Equipo {
-    constructor(nombre, ciudad ,jugadorEstrella, jugadorActual) {
-        this.nombre = nombre;
-        this.ciudad = ciudad;
-        this.jugadorEstrella = jugadorEstrella;
-        this.jugadorActual = jugadorActual;
-    }
-}
-
-
-
-class Entrenador{
-    constructor(nombre, edad, equipo){
-        this.nombre = nombre;
-        this.edad = edad;
-        this.equipo = equipo;
-    }
-}
-
-
