be0f44348ee2de84e308efa41a5fce3651c77ef0
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-09-26 23:23:01 -0500

    Update genertate.js

--- genertate.js
+++ genertate.js
@@ -29,16 +29,3 @@ class Entrenador{
 }
 
 
-var jugador1 = new JugadorFutbol("Lionel Messi", 36, "Delantero");
-var jugador2 = new JugadorFutbol("Erling Haaland", 23, "Delantero");
-var jugador3 = new JugadorFutbol("Kylian Mbappé", 24,  "Delantero");
-var jugador4 = new JugadorFutbol("Robert Lewandowski", 33, "Delantero");
-
-// Instancias de equipos
-var equipo1 = new Equipo("Inter Miami", "Miami", jugador1, jugador2);
-var equipo2 = new Equipo("Manchester City", "Manchester", jugador3, jugador4);
-
-var entrenador1 = new Entrenador("Pep Guardiola", 50, equipo1);
-var entrenador2 = new Entrenador("Jorge Sampaoli", 55, equipo2);
-
-
