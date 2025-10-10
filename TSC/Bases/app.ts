0f9c2aaa4583abb350a792c0fd389aea5fb2101e
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-10-09 20:50:11 -0500

    ih

--- TSC/Bases/app.ts
+++ TSC/Bases/app.ts
@@ -3,3 +3,17 @@ let nickName:string = "Kobe"
 let point: number = 254
 let Teach:string = "Greg Pop"
 
+
+function sayhello( a:number) {
+    console.log(a)
+}
+
+
+
+
+
+((a)=>{
+    console.log(a)
+})(10)
+
+
