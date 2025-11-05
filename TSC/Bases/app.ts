83a2da884a02dcb8a6cbd5f0ce43e15ae797043b
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2025-11-04 22:37:08 -0500

    Update app.ts

---
+++ TSC/Bases/app.ts
@@ -0,0 +1,19 @@
+const msg:string = "Yeffer";
+let nickName:string = "Kobe"
+let point: number = 254
+let Teach:string = "Greg Pop"
+
+
+function sayhello( a:number) {
+    console.log(a)
+}
+
+
+
+
+
+((a)=>{
+    console.log(a)
+})(10)
+
+
