cbebf5d5c023b58351a1c065bb665e3fbe379e5d
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-01-06 21:47:52 -0500

    Update NewDay.js

--- 00-Essentials/NewDay.js
+++ 00-Essentials/NewDay.js
@@ -3,4 +3,5 @@ console.log(`Jun 02th Father mment`)
 console.log(`Jun 03th Talking with the boss`)    
 console.log(`Jun 04th Light Off`)    
 console.log(`Jun 05th Arrived to Mes`)    
+console.log(`Jun 06th Arranca o no Arranca`)    
     
