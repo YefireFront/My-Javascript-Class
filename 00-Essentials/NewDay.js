24e81914ac879410bb5ef11a10b00ce594893a80
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-01-12 23:00:04 -0500

    Update NewDay.js

--- 00-Essentials/NewDay.js
+++ 00-Essentials/NewDay.js
@@ -9,4 +9,5 @@ console.log(`Jun 08th Organize`)
 console.log(`Jun 08th Internet is home`)    
 console.log(`Jun 10th Gina `)    
 console.log(`Jun 11th Ready or not `)    
+console.log(`Jun 11th Cant haen again`)    
     
