bd3cf062f8ce7b8e111596f588e9a0410e753c53
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-01-15 22:15:25 -0500

    Update NewDay.js

--- 00-Essentials/NewDay.js
+++ 00-Essentials/NewDay.js
@@ -12,4 +12,5 @@ console.log(`Jun 11th Ready or not `)
 console.log(`Jun 11th Cant haen again`)    
 console.log(`Jun 12th Talk with Plumaman and close decisione`)    
 console.log(`Jun 13th Back to the project`)    
+console.log(`Jun 13th Back to the project`)    
     
