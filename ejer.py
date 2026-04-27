0f64e244651e3b7dc2e8c57b3f006111684c09ed
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-04-27 16:12:00 -0500

    Create ejer.py

---
+++ ejer.py
@@ -0,0 +1,18 @@
+print("Inicio")
+
+for i in range(0, 11):
+    print(i)
+    
+    if i >= 2 and i <= 4:
+        print("Yeffer es muy pequeño")
+    
+    if i == 5:
+        print("Mitad")
+    
+    if i == 7:
+        print("Cristiano Ronaldo Do Santos")
+    
+    if i == 10:
+        print("Lionel Andres")
+
+print("Fin")
