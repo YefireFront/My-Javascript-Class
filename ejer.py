25cc6b2a89f9ac4a626e3269c771fc83f00ac6f2
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-04-30 23:44:24 -0500

    Update ejer.py

--- ejer.py
+++ ejer.py
@@ -1,18 +1,7 @@
-print("Inicio")
+numero = 0
 
-for i in range(0, 11):
-    print(i)
-    
-    if i >= 2 and i <= 4:
-        print("Yeffer es muy pequeño")
-    
-    if i == 5:
-        print("Mitad")
-    
-    if i == 7:
-        print("Cristiano Ronaldo Do Santos")
-    
-    if i == 10:
-        print("Lionel Andres")
+while numero != 5:
+    numero = int(input("Intenta otra vez: "))
 
-print("Fin")
+print("Saliste del Sistema") 
+   
