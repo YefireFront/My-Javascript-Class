c7ead262edafa1e758887145aa1fe315e93cff88
Yeffer Moreno Robledo <98995975+YefireFront@users.noreply.github.com>
2026-08-27 20:32:33 -0500

    re orgnize

--- TSC/Bases/dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const msg = "Yeffer";
-let nickName = "Kobe";
-let point = 254;
-let Teach = "Greg Pop";
--- TSC/Bases/dist/tipos/booleans.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-(() => {
-    let isSuperMan = true;
-    console.log({ isSuperMan });
-    let nombre = isSuperMan ? "Klarck" : "Kalel";
-    console.log({ nombre });
-})();
